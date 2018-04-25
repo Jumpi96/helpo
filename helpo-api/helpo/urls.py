b0145635403cff838571e779c0ce731c6636501d
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-04-25 19:17:58 -0300

    Ejemplo de tests en common + borrar exampleapp

--- helpo-api/helpo/urls.py
+++ helpo-api/helpo/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from rest_framework import routers
 from actividades import views
-
 import django_js_reverse.views
 
 router = routers.DefaultRouter()
@@ -15,9 +14,7 @@ router.register(r'tipos_de_organizaciones', views.TipoDeOrganizacionViewSet)
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    #url(r'^actividades/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^jsreverse/$', django_js_reverse.views.urls_js, name='js_reverse'),
-    #url(r'^$', TemplateView.as_view(template_name='exampleapp/itworks.html'), name='home'),
 ]
 
 if settings.DEBUG:

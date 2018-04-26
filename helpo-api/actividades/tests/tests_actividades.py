31354284819bf73ed80edadf243bfb2b68053504
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-04-26 17:29:30 -0300

    Nuevos tests

--- helpo-api/actividades/tests/tests_actividades.py
+++ helpo-api/actividades/tests/tests_actividades.py
@@ -10,11 +10,11 @@ client = Client()
 class TipoDeOrganizacionTests(TestCase):
 
     def setUp(self):
-        TipoDeOrganizacion.objects.create(
+        self.ddhh = TipoDeOrganizacion.objects.create(
             nombre = "Defensa de los DDHH"
         )
-        TipoDeOrganizacion.objects.create(
-            nombre = "Defensa de lo s animales"
+        self.peta = TipoDeOrganizacion.objects.create(
+            nombre = "Defensa de los animales"
         )
         
     def test_get_all(self):
@@ -23,3 +23,19 @@ class TipoDeOrganizacionTests(TestCase):
         serializer = TipoDeOrganizacionSerializer(tipos, many=True)
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_existente(self):
+        response = client.get(
+            reverse('tipo_de_organizacion', kwargs={'id': self.ddhh.id})
+        )
+        tipo = TipoDeOrganizacion.objects.get(id=self.ddhh.id)
+        serializer = TipoDeOrganizacionSerializer(tipo)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_no_existente(self):
+        response = client.get(
+            reverse('tipo_de_organizacion', kwargs={'id': 3})
+        )
+        tipo = TipoDeOrganizacion.objects.get(id=self.ddhh.id)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)

d90cb5324742bbfce58dcb7ceccb62622088938c
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-04-27 12:35:44 -0300

    Cambio completo app en React a create-react-app

--- helpo-web/src/index.js
+++ helpo-web/src/index.js
@@ -1,2 +1,8 @@
-// import pages
-import 'pages/homePage';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();

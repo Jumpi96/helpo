568cc0e8d544c47addc2d21440e7a42feb1508e9
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-04-27 09:03:30 -0300

    Cambios en serializers

--- helpo-api/actividades/serializers.py
+++ helpo-api/actividades/serializers.py
@@ -1,23 +1,17 @@
 from rest_framework import serializers
 from actividades.models import Actividad, Organizacion, TipoDeOrganizacion
 
-class ActividadSerializer(serializers.HyperlinkedModelSerializer):
+class ActividadSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actividad
         fields = ('nombre', 'fecha', 'organizacion')
 
-class TipoDeOrganizacionSerializer(serializers.HyperlinkedModelSerializer):
+class TipoDeOrganizacionSerializer(serializers.ModelSerializer):
     class Meta:
         model = TipoDeOrganizacion
         fields = ('nombre',)
 
-class OrganizacionSerializer(serializers.HyperlinkedModelSerializer):
-    tipo = serializers.HyperlinkedRelatedField(
-            queryset='get_put_delete_tipo_de_organizacion',
-            view_name='get_put_delete_tipo_de_organizacion', 
-            lookup_field='id'
-    )
-
+class OrganizacionSerializer(serializers.ModelSerializer):    
     class Meta:
         model = Organizacion
         fields = ('nombre', 'tipo')

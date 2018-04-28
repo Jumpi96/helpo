2bfee406d948ced3ac9d0e158ceac3f546d7c197
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-04-28 12:30:16 -0300

    Agregando componente + estructura repo

--- helpo-mobile/App.js
+++ helpo-mobile/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/common/Header/Header';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Header />
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>

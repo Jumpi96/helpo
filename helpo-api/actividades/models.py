6b0c82c2b2832526325e65cff63e556b8470f0a0
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-05-01 02:56:28 -0300

    Agregado modelo y serializador POST

--- helpo-api/actividades/models.py
+++ helpo-api/actividades/models.py
@@ -8,9 +8,13 @@ class Organizacion(IndexedTimeStampedModel):
     nombre = models.CharField(max_length=100)
     tipo = models.ForeignKey(TipoDeOrganizacion, null=True, on_delete=models.SET_NULL)
 
+class Ubicacion(models.Model):
+    latitud = models.FloatField(null=True)
+    longitud = models.FloatField(null=True)
+    notas = models.CharField(max_length=140)
+
 class Actividad(IndexedTimeStampedModel):
     nombre = models.CharField(max_length=100)
     fecha = models.DateTimeField()
     organizacion = models.ForeignKey(Organizacion, on_delete=models.CASCADE)
-
-
+    ubicacion = models.ForeignKey(Ubicacion, null=True, on_delete=models.SET_NULL)
--- helpo-api/actividades/serializers.py
+++ helpo-api/actividades/serializers.py
@@ -1,10 +1,25 @@
 from rest_framework import serializers
-from actividades.models import Actividad, Organizacion, TipoDeOrganizacion
+from actividades.models import Actividad, Organizacion, TipoDeOrganizacion, Ubicacion
+
+class UbicacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ubicacion
+        fields = ('latitud', 'longitud', 'notas')
 
 class ActividadSerializer(serializers.ModelSerializer):
+    ubicacion = UbicacionSerializer()
+    
     class Meta:
         model = Actividad
-        fields = ('nombre', 'fecha', 'organizacion')
+        fields = ('id', 'nombre', 'fecha', 'organizacion', 'ubicacion')
+
+    def create(self, validated_data):
+        ubicacion_data = validated_data.pop('ubicacion')
+        ubicacion = Ubicacion.objects.create(**ubicacion_data)
+        actividad = Actividad.objects.create(ubicacion=ubicacion, **validated_data)
+        return actividad
+    
+    #TODO: def update
 
 class TipoDeOrganizacionSerializer(serializers.ModelSerializer):
     class Meta:

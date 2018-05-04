374c3fd1ed15eeb24c186a1f815d18023d3ca547
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-05-04 00:01:28 -0300

    Funcionando con react-router-dom

--- helpo-web/src/App.js
+++ helpo-web/src/App.js
@@ -3,18 +3,58 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './common/Header/Header';
 import EventoActividad from './actividades/EventoActividad/EventoActividad'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header logo={logo} />
-        <div className="container">
-          <EventoActividad />
+      <Router>
+        <div className="App">
+          <Header logo={logo} />
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/actividades">Actividades</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/actividades" component={Actividades} />
         </div>
-      </div>
-      
+      </Router>
     );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <h3>This is Home</h3>
+    )
+  }
+}
+
+class Actividades extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Actividades</h3>
+        <ul>
+          <li>
+            <Link to={"actividades/registrarEvento"}>Registrar evento</Link>
+          </li>
+        </ul>
+        
+        <hr/>
+        <Route exact path="/actividades" render={() => <h4>Selecciona una opción</h4>} />
+        <Route path={"/actividades/registrarEvento"} component={EventoActividad} />
+      </div>
+    )
   }
 }
 

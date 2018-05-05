fc9946573f7738dec9107347fefa1e864a87fb8c
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-05-05 19:08:46 -0300

    Selector agregado a SelectorUbicacion mobile

--- helpo-mobile/src/actividades/RegistrarEvento/RegistrarEvento.js
+++ helpo-mobile/src/actividades/RegistrarEvento/RegistrarEvento.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Route, Link } from 'react-router-native';
 import styles from './RegistrarEventoCSS';
+import SelectorUbicacion from './SelectorUbicacion/SelectorUbicacion';
 
 class Actividades extends Component {
     render(){
       return (
         <View>
           <Text>TODO: todo registrar evento</Text>
+          <SelectorUbicacion />
         </View>
       );
     }
---
+++ helpo-mobile/src/actividades/RegistrarEvento/SelectorUbicacion/SelectorUbicacion.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { FormLabel, FormInput, Button } from 'react-native-elements';
+import RNGooglePlacePicker from 'react-native-google-place-picker';
+
+
+class SelectorUbicacion extends Component {
+  constructor(props){
+    super(props);
+    this.handleNotasChange = this.handleNotasChange.bind(this);
+    this.handleCoordenadasChange = this.handleCoordenadasChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChange({ 
+      latitud: this.props.ubicacionPorDefecto.lat,
+      longitud: this.props.ubicacionPorDefecto.lng,
+      notas: ''
+    })
+  }
+
+  handleChange(ubi) {
+    this.props.onUbicacionChange(ubi);
+  }
+
+  handleNotasChange(e){
+    const lat = this.props.ubicacion.latitud;
+    const long = this.props.ubicacion.longitud;
+    const ubi = { latitud: lat,
+                longitud: long,
+                notas: e.target.value
+    };
+    this.handleChange(ubi)
+  }
+
+  handleCoordenadasChange(location){
+    const notas = this.props.ubicacion.notas;
+    const ubi = { latitud: location.latitude,
+                longitud: position.longitude,
+                notas: notas
+    };
+    this.handleChange(ubi);
+  }
+
+  seleccionarUbicacion() {
+    RNGooglePlacePicker.show((response) => {
+      if (!response.didCancel && !response.error)
+        this.handleCoordenadasChange(response);
+    })
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <FormLabel>Ubicación</FormLabel>
+        <Button
+          icon={{name: 'touch-app'}}
+          title='Seleccionar ubicación' 
+          onPress={this.seleccionarUbicacion}/>
+        <FormInput
+          placeholder="Notas (Calle, número, localidad, CP)"
+          value={this.props.ubicacion.notas}
+          onChangeText={this.handleNotasChange} />
+      </View>
+    );
+  }
+}
+
+export default SelectorUbicacion;

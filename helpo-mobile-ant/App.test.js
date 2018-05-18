94239998a275bb0f0c7c678779ffecb24282d998
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-05-18 14:07:19 -0300

    Borrado helpo-mobile-ant

--- helpo-mobile-ant/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import App from './App';
-
-it('renders without crashing', () => {
-  const rendered = shallow(<App />);
-  expect(rendered).toMatchSnapshot();
-});
--- helpo-mobile-ant/src/actividades/RegistrarEvento/SelectorUbicacion/SelectorUbicacionCSS.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({  
-  container: {
-        marginTop: 25,
-        padding: 10,
-    },
-});

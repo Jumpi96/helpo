d5d656610a3de20ca815c99a3948e6f0b83b9c13
Agustin Borello <agustinborello21@gmail.com>
2018-05-20 12:35:31 -0300

    Arregle funcionalidad de remover eventos

--- helpo-web/src/views/Actividades/RegistrarEvento/RegistrarContacto/RegistrarContacto.jsx
+++ helpo-web/src/views/Actividades/RegistrarEvento/RegistrarContacto/RegistrarContacto.jsx
@@ -4,28 +4,69 @@ import PropTypes from 'prop-types';
 const inputPropTypes = {
   contactNum: PropTypes.number.isRequired,
   onClickRemove: PropTypes.func.isRequired,
-  id: PropTypes.number.isRequired,
+  contactId: PropTypes.number.isRequired,
+  onContactChange: PropTypes.func.isRequired,
+  contactValues: PropTypes.shape({
+    nombre: PropTypes.string,
+    mail: PropTypes.string,
+    telefono: PropTypes.string,
+  }).isRequired,
 };
 
-function ContactoInput({ contactNum, onClickRemove, id }) {
+function ContactoInput({
+  contactNum, onClickRemove, contactId, onContactChange, contactValues,
+}) {
   return (
-    <div>
-      <h2>Contacto {contactNum}</h2>
-      <p>Nombre</p>
-      <input type="text" name="nombre" />
-      <p>Mail</p>
-      <input type="text" name="mail" />
-      <p>Telefono</p>
-      <input type="text" name="telefono" />
-      <input type="button" value="X" onClick={() => onClickRemove(id)} />
+    <div className="form-group">
+      <h3>Contacto {contactNum}</h3>
+      <label htmlFor={`nombre-contacto${contactNum}`}>
+        Nombre<br />
+        <input
+          type="text"
+          id={`nombre-contacto${contactNum}`}
+          className="form-control"
+          name="nombre"
+          onChange={e => onContactChange(e, contactId)}
+          value={contactValues.nombre}
+        />
+      </label> <br />
+      <label htmlFor={`mail-contacto${contactNum}`}>
+        Mail<br />
+        <input
+          type="text"
+          id={`mail-contacto${contactNum}`}
+          className="form-control"
+          name="mail"
+          onChange={e => onContactChange(e, contactId)}
+          value={contactValues.mail}
+        />
+      </label> <br />
+      <label htmlFor={`telefono-contacto${contactNum}`}>
+        Telefono<br />
+        <input
+          type="text"
+          id={`telefono-contacto${contactNum}`}
+          className="form-control"
+          name="telefono"
+          onChange={e => onContactChange(e, contactId)}
+          value={contactValues.telefono}
+        />
+      </label> <br />
+      <input
+        type="button"
+        value="Remover Contacto"
+        onClick={() => onClickRemove(contactId)}
+      />
     </div>
-  );}
+  );
+}
 ContactoInput.propTypes = inputPropTypes;
 
 const registerPropTypes = {
   contacts: PropTypes.arrayOf(PropTypes.object).isRequired,
   onClickAdd: PropTypes.func.isRequired,
   onClickRemove: PropTypes.func.isRequired,
+  onContactChange: PropTypes.func.isRequired,
 };
 
 class RegistrarContacto extends React.Component {
@@ -33,14 +74,20 @@ class RegistrarContacto extends React.Component {
     const inputs = [];
     for (let i = 1;
       i <= this.props.contacts.length; i += 1) {
-      inputs.push(<ContactoInput contactNum={i} onClickRemove={this.props.onClickRemove} />);
+      inputs.push(<ContactoInput
+        contactNum={i}
+        onClickRemove={this.props.onClickRemove}
+        contactId={this.props.contacts[i - 1].contactId}
+        onContactChange={this.props.onContactChange}
+        contactValues={this.props.contacts[i - 1]}
+      />);
     }
     return inputs || null;
   }
 
   render() {
     return (
-      <div>
+      <div className="form-group">
         <h1>Registrar Contacto</h1>
         {this.displayInputs()}
         <input type="button" value="Add" onClick={this.props.onClickAdd} />

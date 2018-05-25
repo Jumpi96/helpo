e18b6fc00fe04c3e0908a0182fa096c1c003e265
Juan Pablo Lorenzo <juampilorenzo@gmail.com>
2018-05-25 16:53:15 -0300

    Cambios varios, SelectorItem sigue sin andar bien

--- helpo-web/src/views/Actividades/RegistrarNecesidades/RegistrarNecesidades.js
+++ helpo-web/src/views/Actividades/RegistrarNecesidades/RegistrarNecesidades.js
@@ -2,21 +2,167 @@ import React, { Component } from 'react';
 import { Button, Table, Card, CardHeader, CardBody } from 'reactstrap';
 import './RegistrarNecesidades.css';
 import SelectorItem from './SelectorItem/SelectorItem';
+import api from '../../../api';
 
 class RegistrarNecesidades extends Component {
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      necesidades: [],
+      necesidad: undefined,
+      cantidad: undefined,
+      recurso_id: 0,
+      descripcion: undefined,
+      error: undefined
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleItemChange = this.handleItemChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleSubmit(event) {
     event.preventDefault();
     if (this.handleValidation()) {
+      const necesidad = {
+        recurso_id: this.state.recurso_id,
+        descripcion: this.state.descripcion,
+        cantidad: this.state.cantidad,
+        evento: 2 //this.props.evento
+      }
+      if (!this.state.necesidad){
+        this.addNecesidad(necesidad);
+      } else {
+        necesidad.id = this.state.necesidad;
+        this.saveNecesidad(necesidad);
+      }
+    }
+  }
+
+  addNecesidad(necesidad) {
+    api.post('/actividades/necesidades/', necesidad)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        this.cleanNecesidad();
+        this.loadNecesidades();
+      }).catch(function (error) {
+        if (error.response){ console.log(error.response.status) }
+        else { console.log('Error: ', error.message)}
+      });
+  }
+
+  saveNecesidad(necesidad) {
+    api.put("/actividades/necesidades/" + necesidad.id + "/", necesidad)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        this.cleanNecesidad();
+        this.loadNecesidades();
+      }).catch(function (error) {
+        if (error.response){ console.log(error.response.status) }
+        else { console.log('Error: ', error.message)}
+      });
+  }
+
+  handleValidation() {
+    let formIsValid = true;
+    var error = this.state.error;
+
+    if (this.state.recurso_id === 0 || 
+      !this.state.recurso_id) {
+      formIsValid = false;
+      error = "Hubo un problema al cargar los recursos.";
+    } else {
+      error = undefined;
+    }
+    this.setState({error: error});
+    return formIsValid;
+  }
+
+  editNecesidad(id) {
+    const necesidad = this.state.necesidades.filter(n => n.id === id)[0];
+    this.setState({ 
+      descripcion: necesidad.descripcion,
+      cantidad: necesidad.cantidad,
+      recurso_id: necesidad.recurso.id,
+      necesidad: necesidad.id
+    });
+  }
+
+  cleanNecesidad() {
+    this.setState({
+      descripcion: undefined,
+      cantidad: undefined,
+      recurso_id: undefined,
+      necesidad: undefined
+    });
+  }
+
+  deleteNecesidad(necesidad) {
+    api.delete('/actividades/necesidades/' + necesidad)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+        this.cleanNecesidad();
+        this.loadNecesidades();
+      }).catch(function (error) {
+        if (error.response){ console.log(error.response.status) }
+        else { console.log('Error: ', error.message)}
+      });
+  }
+
+  componentDidMount() {
+    this.loadNecesidades();
+  }
+
+  loadNecesidades() {
+    api.get('/actividades/necesidades/?evento=' + 2)//this.props.evento_id)
+      .then(res => {
+        const necesidadesData = res.data;
+        this.setState({ necesidades: necesidadesData});
+      })
+      .catch(function (error) {
+        if (error.response){ console.log(error.response.status) }
+        else { console.log('Error: ', error.message)}
+      })
+  }
+
+  handleItemChange(r) {
+    this.setState({ recurso_id: parseInt(r) });
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+        [name]: value
+    });
+  }
+
+  getSelectorClass(id){
+    if (this.state.necesidad === id) {
+      return "fa fa-arrow-circle-o-right";
+    } else {
+      return undefined;
     }
   }
 
   render() {
+    const tablaNecesidades = this.state.necesidades.map((n) =>
+      <tr>
+        <td><i className={this.getSelectorClass(n.id)}></i></td>
+        <td><i className={n.recurso.categoria.icono}></i></td>
+        <td>{n.recurso.categoria.nombre}</td>
+        <td>{n.recurso.nombre}</td>
+        <td>{n.descripcion}</td>
+        <td>{n.cantidad}</td>
+        <Button onClick={() => this.editNecesidad(n.id)} outline
+          disabled={this.state.necesidad} color="warning">Editar</Button>
+        <Button onClick={() => this.deleteNecesidad(n.id)} outline 
+          disabled={this.state.necesidad} color="danger">Eliminar</Button>
+      </tr>
+    );
     return (
       <div className="animated fadeIn">
         <Card>
@@ -25,61 +171,52 @@ class RegistrarNecesidades extends Component {
           </CardHeader>
           <CardBody>
             <div className="form-group">
-              <div className="row">
-                <div className="col-md-4">
-                  <SelectorItem />
+              <form onSubmit={this.handleSubmit}>
+                <div className="row">
+                  <div className="col-md-4">
+                    <SelectorItem 
+                     item={this.state.recurso_id}
+                     onItemChange={this.handleItemChange}/>
+                  </div>
+                  <div className="col-md-2">
+                    <input type="text" 
+                      name="cantidad" className="form-control"
+                      placeholder="Cantidad"
+                      value={this.state.cantidad} 
+                      onChange={this.handleInputChange}
+                    />
+                  </div>
+                  <div className="col-md-4">
+                    <input type="text" 
+                      name="descripcion" className="form-control"
+                      placeholder="Descripción"
+                      value={this.state.descripcion} 
+                      onChange={this.handleInputChange}
+                    />
+                  </div>
+                  <div className="col-md-2">
+                    <Button outline type="submit" color="success">
+                      { !this.state.necesidad ? "Agregar" : "Guardar" }
+                    </Button>
+                  </div>
                 </div>
-                <div className="col-md-2">
-                  <input type="text" 
-                    name="cantidad" className="form-control"
-                    placeholder="Cantidad"
-                    value={this.state.cantidad} 
-                    onChange={this.handleInputChange}
-                  />
-                </div>
-                <div className="col-md-4">
-                  <input type="text" 
-                    name="descripcion" className="form-control"
-                    placeholder="Descripción"
-                    value={this.state.descripcion} 
-                    onChange={this.handleInputChange}
-                  />
-                </div>
-                <div className="col-md-2">
-                  <Button outline color="success">Agregar</Button>
-                </div>
-              </div>
+                <span style={{color: "red"}}>{this.state.error}</span>
+              </form>
             </div>
             <Table responsive striped>
               <thead>
-              <tr>
-                <th></th>
-                <th>Categoría</th>
-                <th>Ítem</th>
-                <th>Descripción</th>
-                <th>Cantidad</th>
-                <th></th>
-              </tr>
+                <tr>
+                  <th></th>
+                  <th></th>
+                  <th>Categoría</th>
+                  <th>Ítem</th>
+                  <th>Descripción</th>
+                  <th>Cantidad</th>
+                  <th></th>
+                </tr>
               </thead>
               <tbody>
-              <tr>
-                <td><i className="cui-bell icons"></i></td>
-                <td>Ropa</td>
-                <td>Sweater</td>
-                <td>Tiene que ser rojo.</td>
-                <td>1</td>
-                <Button outline color="warning">Editar</Button>
-                <Button outline color="danger">Eliminar</Button>
-              </tr>
-              <tr>
-                <td><i className="cui-equalizer icons"></i></td>
-                <td>Ropa</td>
-                <td>Sweater</td>
-                <td>Tiene que ser rojo.</td>
-                <td>1</td>
-                <Button outline color="warning">Editar</Button>
-                <Button outline color="danger">Eliminar</Button>
-              </tr>
+                {tablaNecesidades}
               </tbody>
             </Table>
 
--- helpo-web/src/views/Actividades/RegistrarNecesidades/SelectorItem/SelectorItem.js
+++ helpo-web/src/views/Actividades/RegistrarNecesidades/SelectorItem/SelectorItem.js
@@ -5,23 +5,36 @@ import api from '../../../../api';
 class SelectorItem extends Component {
   constructor(props){
     super(props);
-    this.state = { 
-      rubros: [{ id: 0, nombre: 'Sin rubros' }]
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(e) {
-    const selectedIndex = e.target.options.selectedIndex;
-    const selectedId = e.target.options[selectedIndex].getAttribute('data-key');
-    this.props.onRubroChange(selectedId);
+    this.state = {
+        categorias: [{ id: 0, nombre: 'Sin categorías' }],
+        items: [{ id: 0, nombre: 'Sin ítems' }],
+        categoria_id: 0
+      };
+    this.handleSelectItem = this.handleSelectItem.bind(this); 
+    this.handleChangeCategoria = this.handleChangeCategoria.bind(this);
   }
 
-  componentDidMount(){
-    api.get('/actividades/rubros_evento')
+  handleSelectItem(e) {
+    const selectedIndex = e.target.options.selectedIndex;
+    const selectedId = e.target.options[selectedIndex].getAttribute('data-key');
+    this.handleChangeItem(selectedId);
+  }
+  
+  handleChangeItem(selectedId){
+    this.props.onItemChange(selectedId);
+  }
+
+  handleChangeCategoria(e) {
+    const selectedIndex = e.target.options.selectedIndex;
+    const selectedId = e.target.options[selectedIndex].getAttribute('data-key');
+    this.setState({ categoria_id: selectedId});
+  }
+
+  getCategoriaByItem(item) {
+    api.get('/actividades/recursos/' + item + '/')
       .then(res => {
-        const rubrosData = res.data;
-        this.setState({ rubros: rubrosData});
-        this.props.onRubroChange(rubrosData[0].id);
+        const recursoData = res.data;
+        return recursoData.categoria.id;
       })
       .catch(function (error) {
         if (error.response){ console.log(error.response.status) }
@@ -29,20 +42,95 @@ class SelectorItem extends Component {
       })
   }
 
-  render(){
-    const listaRubroEventos = this.state.rubros.map((r) =>
-      <option key={r.id} data-key={r.id}>{r.nombre}</option>
+  getItems(categoria) {
+    api.get('/actividades/recursos/?categoria=' + categoria)
+      .then(res => {
+        const recursosData = res.data;
+        return recursosData;
+      })
+      .catch(function (error) {
+        if (error.response){ console.log(error.response.status) }
+        else { console.log('Error: ', error.message) };
+      })
+  }
+
+  componentDidMount() {
+    let listaCategorias, selectedCategoria;
+    api.get('/actividades/categorias_recurso')
+      .then(res => {
+        const categoriasData = res.data;
+        listaCategorias = categoriasData;
+        selectedCategoria = categoriasData[0].id;
+        api.get('/actividades/recursos/?categoria=' + selectedCategoria)
+          .then(res => {
+            const recursosData = res.data;
+            this.setState({ categorias:listaCategorias, categoria_id: selectedCategoria, items: recursosData});
+          })
+          .catch(function (error) {
+            if (error.response){ console.log(error.response.status) }
+            else { console.log('Error: ', error.message) };
+          })
+      })
+      .catch(function (error) {
+        if (error.response){ console.log(error.response.status) }
+        else { console.log('Error: ', error.message) };
+      })
+  }
+
+ 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    let selectedCategoria = prevState.categoria_id;
+    let listaItems = prevState.items;
+    if (prevState.categoria_id !== 0) {
+      api.get('/actividades/recursos/' + nextProps.item + '/')
+      .then(res => {
+        const recursoData = res.data;
+        selectedCategoria = recursoData.categoria.id;
+        api.get('/actividades/recursos/?categoria=' + selectedCategoria)
+          .then(res => {
+            listaItems = res.data;
+            return { categoria_id: selectedCategoria, items: listaItems, categorias: prevState.categorias };
+          })
+      });
+    } else {
+      return { categoria_id: selectedCategoria, items: listaItems, categorias: prevState.categorias };
+    }
+  }
+
+  render() {
+    const listaCategorias = this.state.categorias.map((r) =>
+      <option value={r.id} key={r.id} data-key={r.id}>{r.nombre}</option>
     );
-    const rubro = this.props.rubro_id;
+    const listaItems = this.state.items.map((i) =>
+      <option value={i.id} key={i.id} data-key={i.id}>{i.nombre}</option>
+    );
+    //alert(this.state.categoria_id + "a" + this.props.item);
     return(
-      <select
-        value={rubro}
-        className="form-control"
-        onChange={this.handleChange}>
-          {listaRubroEventos}
-      </select>
+      <div className="row">
+        <div className="col-md-6">
+          <select
+            value={this.state.categoria_id}
+            className="form-control"
+            onChange={this.handleChangeCategoria}>
+              {listaCategorias}
+          </select>
+        </div>
+        <div className="col-md-6">
+          <select
+            value={this.props.item}
+            className="form-control"
+            onChange={this.handleSelectItem}>
+            {listaItems}
+          </select>
+        </div>
+      </div>
     )
   }
 }
 
+SelectorItem.defaultProps = {
+  item: 0
+}
+
 export default SelectorItem;
+

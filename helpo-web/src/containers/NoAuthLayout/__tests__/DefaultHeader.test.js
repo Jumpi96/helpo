f88d5d67985ec8d3ae87549de26fdb52e33891bf
= <=>
2018-06-17 22:24:45 -0300

    More bugs fixed

--- helpo-web/src/containers/NoAuthLayout/__tests__/DefaultHeader.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NoAuthHeader from '../NoAuthHeader';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<NoAuthHeader />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});

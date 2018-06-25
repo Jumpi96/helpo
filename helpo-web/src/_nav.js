0c9b6376c5e558f6bfbe9020b15bcd35621d3857
Luciano Gil <luchogil96@gmail.com>
2018-06-25 12:41:24 -0300

    Navegacion

--- helpo-web/src/_nav.js
+++ helpo-web/src/_nav.js
@@ -1,4 +1,4 @@
-export default {
+  export default {
   items: [
     {
       name: 'Dashboard',
@@ -17,6 +17,20 @@ export default {
     {
       name: 'Nuevo evento',
       url: '/actividades/registrar-evento',
+      icon: 'icon-pencil',
+    },
+    {
+      title: true,
+      name: 'Perfil',
+      wrapper: {            // optional wrapper object
+        element: '',        // required valid HTML5 element tag
+        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+      },
+      class: ''             // optional class names space delimited list for title item ex: "text-center"
+    },
+    {
+      name: 'Modificar perfil',
+      url: '/perfiles/perfil-organizacion',
       icon: 'icon-pencil',
     },
     {

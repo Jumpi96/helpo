83154b9aaaad91fa40ec288ee55fbce3101025e6
Luciano Gil <luchogil96@gmail.com>
2018-07-01 20:50:17 -0300

    .

--- helpo-web/src/containers/DefaultLayout/DefaultHeader.js
+++ helpo-web/src/containers/DefaultLayout/DefaultHeader.js
@@ -63,9 +63,10 @@ class DefaultHeader extends Component {
               <DropdownItem><i className="fa fa-tasks"></i> Tasks<Badge color="danger">42</Badge></DropdownItem>
               <DropdownItem><i className="fa fa-comments"></i> Comments<Badge color="warning">42</Badge></DropdownItem>
               <DropdownItem header tag="div" className="text-center"><strong>Settings</strong></DropdownItem>
-              <DropdownItem onClick={<Link to="../../views/Perfiles/ConsultarPerfilGenerico  ">About</Link>}><i className="fa fa-user"></i> Profile</DropdownItem> {/*Deberia rutear a la consulta de perfil generico*/}
+              <DropdownItem onClick={<Link to="../../views/Perfiles/ConsultarPerfilGenerico  ">Perfil</Link>}><i className="fa fa-user"></i> Perfil</DropdownItem> {/*Deberia rutear a la consulta de perfil generico*/}
               <DropdownItem><i className="fa fa-wrench"></i> Settings</DropdownItem>
-              <DropdownItem><i className="fa fa-usd"></i> Payments<Badge color="secondary">42</Badge></DropdownItem>
+              <DropdownItem><i className="fa fa-usd">
+              </i> Payments<Badge color="secondary">42</Badge></DropdownItem>
               <DropdownItem><i className="fa fa-file"></i> Projects<Badge color="primary">42</Badge></DropdownItem>
               <DropdownItem divider />
               <DropdownItem onClick={() => this.props.logout()}><i className="fa fa-lock"></i> Cerrar sesión</DropdownItem>

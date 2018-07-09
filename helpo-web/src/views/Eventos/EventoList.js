a2e218f90328ca6e376f31996d2827f4d4975fb8
Jumpi96 <juampilorenzo@gmail.com>
2018-07-09 13:09:53 -0300

    Funcionando en web

--- helpo-web/src/views/Eventos/EventoList.js
+++ helpo-web/src/views/Eventos/EventoList.js
@@ -5,8 +5,18 @@ import Widget02 from '../Widgets/Widget02';
 
 class EventoList extends React.Component {
   
+  sortEventos(eventos) {
+    return eventos.sort(function(a, b) {
+      var keyA = new Date(a.fecha_hora_inicio),
+      keyB = new Date(b.fecha_hora_inicio);
+      if(keyA < keyB) return 1;
+      if(keyA > keyB) return -1;
+      return 0;
+    });
+  }
+
   render() {
-    const eventos = this.props.eventos;
+    const eventos = this.sortEventos(this.props.eventos);
     return (
       <ul className="list-group">
         {eventos.map(evento => 

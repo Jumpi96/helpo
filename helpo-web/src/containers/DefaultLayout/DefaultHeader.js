f7aab8c974fb96f7af13fa6a27bb84475ed9bb6a
Luciano Gil <luchogil96@gmail.com>
2018-07-15 18:47:48 -0300

    .

--- helpo-web/src/containers/DefaultLayout/DefaultHeader.js
+++ helpo-web/src/containers/DefaultLayout/DefaultHeader.js
@@ -64,7 +64,9 @@ class DefaultHeader extends Component {
               <DropdownItem><i className="fa fa-tasks"></i> Tasks<Badge color="danger">42</Badge></DropdownItem>
               <DropdownItem><i className="fa fa-comments"></i> Comments<Badge color="warning">42</Badge></DropdownItem>
               <DropdownItem header tag="div" className="text-center"><strong>Settings</strong></DropdownItem>
-              <DropdownItem onClick={<Link to="../../../src/views/Perfiles/ConsultarPerfilGenerico">Perfil</Link>}><i className="fa fa-user"></i> Perfil</DropdownItem> {/*Deberia rutear a la consulta de perfil generico*/}
+              <DropdownItem onClick={<Link to="../../../src/views/Perfiles/ConsultarPerfilGenerico">Perfil</Link>}><i className="fa fa-user"></i> 
+              
+              Perfil</DropdownItem> {/*Deberia rutear a la consulta de perfil generico*/}
               <DropdownItem><i className="fa fa-wrench"></i> Settings</DropdownItem>
               <DropdownItem><i className="fa fa-usd">
               </i> Payments<Badge color="secondary">42</Badge></DropdownItem>

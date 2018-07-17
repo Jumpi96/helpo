ba27bb2585fd67a4129b4b7e7a655a60b28e3e0b
Luciano Gil <luchogil96@gmail.com>
2018-07-17 20:27:48 -0300

    Avances en el nuevo componente para contactos.

--- helpo-web/src/views/Actividades/RegistrarEvento/RegistrarContacto/ModalEditarItem.js
+++ helpo-web/src/views/Actividades/RegistrarEvento/RegistrarContacto/ModalEditarItem.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
-import validateEmail from '../../../../utils/ValidateEmail'
+import NumericInput from 'react-numeric-input';
 
 class ModalEditarItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
       error: undefined,
-      contactos: this.props.contactos
+      contactos: this.props.contactos,
+      contactoId: this.props.contactoId
     };
     this.handleCantidadChange = this.handleCantidadChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,86 +25,67 @@ class ModalEditarItem extends Component {
 
   handleSubmit() {
     if (this.handleValidation()) {
-      this.props.closeModal(this.state.contactoModificado);
+      this.props.closeModal(this.state.cantidad);
     }
   }
 
-  handleValidation() { 
-    let formIsValid = true;
-    var error = this.state.error;    
-    if (this.state.nombre === "") {
-        error = 'No puede ingresar un contacto sin nombre';        
-        formIsValid = false;
-      }
-    if (this.state.mail === "" && this.state.telefono === "") {
-        error += ' Debe ingresar un mail o un telefono';        
-        formIsValid = false;
-      }
-    if (this.state.mail !== "" && !validateEmail(this.state.mail)) {
-        error += ' Debe ingresar un mail valido';        
-        formIsValid = false;
-    }
-    this.setState({error: error});
-    return formIsValid;      
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-    this.setState({
-        [name]: value
-    });
+  handleValidation() {
+    return true;
   }
 
   render() {
-    //const nombre = this.props.contacto ? this.props.contacto.nombre : undefined;
-    //const email = this.props.contacto ? this.props.contacto.email : undefined;
-    //const telefono = this.props.contacto ? this.props.contacto.telefono : undefined;
+    const nombre = this.props.contacto ? this.props.contacto.nombre : undefined;
+    const email = this.props.contacto ? this.props.contacto.email : undefined;
+    const telefono = this.props.contacto ? this.props.contacto.telefono : undefined;
     return (
       <div className="animated fadeIn">
         <Modal isOpen={this.props.open}
           className='modal-warning'>
-
           <ModalHeader>Editar contacto</ModalHeader>
-
           <ModalBody>
-            <div className="row">            
-                  <div className="col-md-3">
-                  <input type="text" 
-                      name="nombre" className="form-control"
-                      placeholder="Nombre"
-                      value={this.props.contacto.nombre} 
-                      onChange={this.handleInputChange}
-                    />
-                  </div>
-                  <div className="col-md-3">
-                  <input type="text" 
-                      name="email" className="form-control"
-                      placeholder="email"
-                      value={this.props.contacto.email} 
-                      onChange={this.handleInputChange}
-                    />
-                  </div>
-                  <div className="col-md-3">
-                    <input type="text" 
-                      name="telefono" className="form-control"
-                      placeholder="Teléfono"
-                      value={this.props.contacto.telefono} 
-                      onChange={this.handleInputChange}
-                    />
-                  </div>
+            <div className="row">
+              <div className="col-md-4">
                 <div>
+                  <strong>Nombre</strong>
+                </div>
+                <div>
+                  <label>{this.props.contactos}</label>
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div>  
+                  <strong>e-Mail</strong>
+                </div>
+                <div>
+                  <label>{email}</label>
+                </div>
+              </div>            
+              <div className="col-md-4">
+                <div>
+                  <strong>Teléfono</strong>
+                </div>
+                <div>
+                  {telefono}
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <div>
+                  <strong>Cantidad</strong>
+                </div>
+                <div>
+                  <NumericInput className="form-control" min="1"
+                    value={this.state.cantidad} onChange={this.handleCantidadChange}/>
                   <span style={{color: "red"}}>{this.state.error}</span>
-                </div>              
+                </div>
+              </div>
             </div>
           </ModalBody>
-
           <ModalFooter>
             <Button color="warning" onClick={this.handleSubmit}>Editar</Button>{' '}
             <Button color="secondary" onClick={() => this.props.closeModal(undefined)}>Cancelar</Button>
           </ModalFooter>
-
         </Modal>
       </div>
     );

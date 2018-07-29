3b998c042b48561f8a61f706b1658579d718443f
Jumpi96 <juampilorenzo@gmail.com>
2018-07-29 11:00:23 -0300

    Rutas separadas web

---
+++ helpo-web/src/_navOrganizacion.js
@@ -0,0 +1,53 @@
+export default {
+    items: [
+      {
+        name: 'Dashboard',
+        url: '/dashboard',
+        icon: 'icon-speedometer'
+      },
+      {
+        title: true,
+        name: 'Actividades sociales',
+        wrapper: {            // optional wrapper object
+          element: '',        // required valid HTML5 element tag
+          attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+        },
+        class: ''             // optional class names space delimited list for title item ex: "text-center"
+      },
+      {
+        name: 'Eventos',
+        url: '/actividades/consultar-eventos',
+        icon: 'icon-cursor'
+      },
+      {
+        name: 'Nuevo evento',
+        url: '/actividades/registrar-evento',
+        icon: 'icon-pencil',
+      },
+      {
+        name: 'Mis eventos',
+        url: '/actividades/evento',
+        icon: 'icon-star'
+      },
+      {
+        title: true,
+        name: 'Perfil',
+        wrapper: {            // optional wrapper object
+          element: '',        // required valid HTML5 element tag
+          attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
+        },
+        class: ''             // optional class names space delimited list for title item ex: "text-center"
+      },
+      {
+        name: 'Modificar perfil',
+        url: '/perfiles/perfil-organizacion',
+        icon: 'icon-pencil',
+      },
+      {
+        name: 'Consultar perfil',
+        url: '/perfil',
+        icon: 'icon-pencil',
+      },
+    ],
+  };
+  
---
+++ helpo-web/src/routesVoluntario.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Loadable from 'react-loadable'
+
+import DefaultLayout from './containers/DefaultLayout';
+
+function Loading() {
+  return <div>Cargando...</div>;
+}
+
+const ConsultarEventosPage = Loadable({
+  loader: () => import('./views/Actividades/ConsultarEventos/ConsultarEventosPage.js'),
+  loading: Loading,
+})
+
+const ConsultarEventosView = Loadable({
+  loader: () => import('./views/Actividades/ConsultarEventos/ConsultarEventosView'),
+  loading: Loading,
+})
+
+const RegistrarColaboraciones = Loadable({
+  loader: () => import('./views/Actividades/ConsultarEventos/RegistrarColaboraciones/RegistrarColaboraciones'),
+  loading: Loading,
+});
+
+const ConsultarPerfil = Loadable({
+  loader: () => import('./views/Perfiles/ConsultarPerfilGenerico'),
+  loading: Loading,
+});
+
+const Dashboard = Loadable({
+  loader: () => import('./views/Dashboard'),
+  loading: Loading,
+});
+
+
+// https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
+const routes = [
+  { path: '/', exact: true, name: 'Home', component: DefaultLayout },
+  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/actividades/consultar-eventos', name: 'Consultar eventos', component: ConsultarEventosPage },
+  { path: '/actividades/consultar-evento/:id', name: 'Consultar evento', component: ConsultarEventosView },
+  { path: '/actividades/registrar-colaboraciones', name: 'Registrar colaboraciones', component: RegistrarColaboraciones },
+  { path: '/perfil', name: 'Mi perfil', component: ConsultarPerfil },
+];
+
+export default routes;

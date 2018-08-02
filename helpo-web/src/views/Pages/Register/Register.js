77900541185467742133724f32cf88eedeff07fc
Luciano Gil <luchogil96@gmail.com>
2018-08-02 14:48:35 -0300

    Intento fallido de modal para confirmar registro y bienvendia al sistema, pero se muestra un alert, que redirige al dashboard si el registro es exitoso.

--- helpo-web/src/views/Pages/Register/Register.js
+++ helpo-web/src/views/Pages/Register/Register.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 //import {auth} from "../../../actions"; SE USA ABAJO - COMENTADO
 import validateEmail from "../../../utils/ValidateEmail";
 import api from "../../../api"
+import ModalRegistroExitoso from './ModalRegistroExitoso';
 
 
 class Register extends Component {
@@ -21,7 +22,8 @@ class Register extends Component {
         apellido: "",
         email: "",
         contraseña: "",
-      }
+      },
+      showModalRegistro: false,
     }
     this.handleUserTypeSelect = this.handleUserTypeSelect.bind(this);
     this.renderNameField = this.renderNameField.bind(this); 
@@ -46,6 +48,11 @@ class Register extends Component {
                         this.state.apellido);*/
       api.post('/auth/sign_up/', usuario)
       .then((res) => {console.log(res)})
+      this.props.history.push('dashboard')
+      alert("Se ha registrado exitosamente en Helpo! \nEsperamos ansiosos, que pueda empezar a disfrutar de la plataforma.");
+      //this.setState({
+        //showModalRegistro:true,
+      //});
     } else {
       // TODO
     }
@@ -305,6 +312,7 @@ class Register extends Component {
             </Col>
           </Row>
         </Container>
+        <ModalRegistroExitoso open={this.state.showModalRegistro}/>
       </div>
     );
   }

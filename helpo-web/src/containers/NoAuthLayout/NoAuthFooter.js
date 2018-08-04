967a304be5ffe3871dcba1780eabfdff3f8b927f
Jumpi96 <juampilorenzo@gmail.com>
2018-08-04 12:12:31 -0300

    Cambios template Web

--- helpo-web/src/containers/NoAuthLayout/NoAuthFooter.js
+++ helpo-web/src/containers/NoAuthLayout/NoAuthFooter.js
@@ -16,7 +16,6 @@ class NoAuthFooter extends Component {
     return (
       <React.Fragment>
         <span><a href="https://coreui.io">CoreUI</a> &copy; 2018 creativeLabs.</span>
-        <span className="ml-auto">Powered by <a href="https://coreui.io/react">CoreUI for React</a></span>
       </React.Fragment>
     );
   }

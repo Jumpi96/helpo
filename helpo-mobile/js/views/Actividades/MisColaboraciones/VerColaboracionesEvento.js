017ad08fa53dc6883dfbe79b5bdf765f09077bc7
Jumpi96 <juampilorenzo@gmail.com>
2018-08-04 23:00:35 -0300

    Finished mobile

--- helpo-mobile/js/views/Actividades/MisColaboraciones/VerColaboracionesEvento.js
+++ helpo-mobile/js/views/Actividades/MisColaboraciones/VerColaboracionesEvento.js
@@ -7,6 +7,7 @@ import {
   Container,
   Header,
   Left,
+  Right,
   Body,
   Title,
   Content,
@@ -26,10 +27,103 @@ class VerColaboracionesEvento extends React.Component {
     evento: React.PropTypes.object,
   }
 
-  render() { 
+  getListaNecesidades() {
+    const listaNecesidades = [];
+    let cantidad;
+    this.props.evento.necesidades.forEach((n) => {
+      cantidad = this.getCantidadNecesidades(n);
+      if (cantidad > 0) {
+        listaNecesidades.push(
+          <ListItem icon key={n.id}>
+            <Left>
+              <Button style={{ backgroundColor: '#4286f4' }}>
+                <Icon active name="archive" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>
+                {n.recurso.nombre} - {n.recurso.categoria.nombre}
+              </Text>
+              <Text numberOfLines={1} note>
+                {n.descripcion}
+              </Text>
+            </Body>
+            <Right>
+              <Text>{cantidad}</Text>
+            </Right>
+          </ListItem>
+        );
+      }
+    });
+    if (listaNecesidades.length > 0) {
+      return (
+        <View>
+          <Separator bordered noTopBorder>
+            <Text>Necesidades</Text>
+          </Separator>
+          {listaNecesidades}
+        </View>
+      );
+    }
+    return undefined;
+  }
+
+  getFuncionVoluntario(voluntarios) {
+    const userId = this.getUserId();
+    for (let i = 0; i < voluntarios.length; i += 1) {
+      if (voluntarios[i].participaciones.filter(p => p.voluntario_id === userId)) {
+        return voluntarios[i];
+      }
+    }
+    return undefined;
+  }
+
+  getCantidadNecesidades(n) {
+    let contador = 0;
+    let userId = this.getUserId();
+    n.colaboraciones.forEach((c) => {
+      if (c.voluntario_id === userId) {
+        contador += c.cantidad;
+      };
+    });
+    return contador;
+  }
+
+  getUserId() {
+    return this.props.auth.user.id;
+  }
+
+  getListaVoluntarios() {
+    const voluntario = this.getFuncionVoluntario(this.props.evento.voluntarios);
+    if (voluntario) {
+      return (
+        <View>
+          <Separator bordered noTopBorder>
+            <Text>Necesidades</Text>
+          </Separator>
+          <ListItem icon key={voluntario.id}>
+            <Left>
+              <Button style={{ backgroundColor: '#ea8f3a' }}>
+                <Icon active name="person" />
+              </Button>
+            </Left>
+            <Body>
+              <Text>
+                {voluntario.funcion.nombre}
+              </Text>
+              <Text numberOfLines={1} note>
+                {voluntario.descripcion}
+              </Text>
+            </Body>
+          </ListItem>
+        </View>
+      );
+    }
+    return undefined;
+  }
+
+  render() {
     const evento = this.props.evento;
-    let listaVoluntarios;
-    let listaNecesidades;
     return (
       <Container style={styles.container}>
         <Header>
@@ -41,6 +135,13 @@ class VerColaboracionesEvento extends React.Component {
           <Body>
             <Title>{evento.nombre}</Title>
           </Body>
+          { moment(evento.fecha_hora_inicio) > moment() ?
+            <Right>
+              <Button transparent onPress={() => Actions.registrarColaboraciones({ evento: evento.id })}>
+                <Text>Modificar</Text>
+              </Button>
+            </Right> : undefined
+          }
         </Header>
         <Content>
           <Separator bordered noTopBorder>
@@ -50,24 +151,8 @@ class VerColaboracionesEvento extends React.Component {
             <Label style={styles.label}>Nombre</Label>
             <Text>{evento.nombre}</Text>
           </ListItem>
-          {listaNecesidades ? (
-            <View>
-              <Separator bordered noTopBorder>
-                <Text>Contactos</Text>
-              </Separator>
-              {listaContactos}
-            </View>
-            ) : undefined
-          }
-          {listaVoluntarios ? (
-            <View>
-              <Separator bordered noTopBorder>
-                <Text>Contactos</Text>
-              </Separator>
-              {listaContactos}
-            </View>
-            ) : undefined
-          }
+          {this.getListaNecesidades()}
+          {this.getListaVoluntarios()}
         </Content>
       </Container>
     );
@@ -84,6 +169,7 @@ function bindAction(dispatch) {
 const mapStateToProps = state => ({
   navigation: state.cardNavigation,
   themeState: state.drawer.themeState,
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps, bindAction)(VerColaboracionesEvento);

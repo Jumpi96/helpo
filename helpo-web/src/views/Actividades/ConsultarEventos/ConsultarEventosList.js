8c5734d854b2d35806385a3afa1b6346e000a818
Jumpi96 <juampilorenzo@gmail.com>
2018-08-06 23:42:34 -0300

    Bug de consultarEventos

--- helpo-web/src/views/Actividades/ConsultarEventos/ConsultarEventosList.js
+++ helpo-web/src/views/Actividades/ConsultarEventos/ConsultarEventosList.js
@@ -6,40 +6,41 @@ import EventoCard from './EventoCard/EventoCard';
 class ConsultarEventosList extends React.Component {
   
   sortEventos(eventos) {
-    if (eventos) {
-      return eventos.sort(function(a, b) {
-        var keyA = new Date(a.fecha_hora_inicio),
-        keyB = new Date(b.fecha_hora_inicio);
-        if(keyA < keyB) return 1;
-        if(keyA > keyB) return -1;
-        return 0;
-      });
-    }
-    return undefined;
+    return eventos.sort(function(a, b) {
+      var keyA = new Date(a.fecha_hora_inicio),
+      keyB = new Date(b.fecha_hora_inicio);
+      if(keyA < keyB) return 1;
+      if(keyA > keyB) return -1;
+      return 0;
+    });
   }
 
   render() {
-    const eventos = this.sortEventos(this.props.eventos);
-    return (
-      <Row>
-        <div className="col-md-3">
-          <Badge color="warning">Próximamente...</Badge>
-        </div>
-        <div className="col-md-9">
-          {eventos.map(evento =>       
-              <Col>
-                <EventoCard
-                  evento={evento}
-                  key={evento.id} footer
-                  color="primary" auth={this.props.auth}
-                  link={'/actividades/consultar-evento/' + evento.id}
-                />
-              </Col>
-          )}
-        </div>
-      </Row>
-    );
-  }
+    if (this.props.eventos.length > 0) {
+      const eventos = this.sortEventos(this.props.eventos);
+      return (
+        <Row>
+          <div className="col-md-3">
+            <Badge color="warning">Próximamente...</Badge>
+          </div>
+          <div className="col-md-9">
+            {eventos.map(evento =>       
+                <Col>
+                  <EventoCard
+                    evento={evento}
+                    key={evento.id} footer
+                    color="primary" auth={this.props.auth}
+                    link={'/actividades/consultar-evento/' + evento.id}
+                  />
+                </Col>
+              )}
+            </div>
+          </Row>
+        );
+    } else {
+      return <p>Cargando...</p>
+    }
+  }  
 };
 
 ConsultarEventosList.propTypes = {  

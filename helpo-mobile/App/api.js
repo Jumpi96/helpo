3d2d59783e590011734f90fbdc134a7c66f467af
GonzaloUlla <gonzaulla@gmail.com>
2018-08-18 18:44:22 -0300

    Setting API-mobile communication

--- helpo-mobile/App/api.js
+++ helpo-mobile/App/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  //baseURL: 'http://10.0.2.2:8000',
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'http://10.0.2.2:8000',
+  //baseURL: 'https://www.helpo.com.ar:8443/',
 });
 
 api.interceptors.request.use(

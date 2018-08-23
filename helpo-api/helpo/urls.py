3893c58a878d7497877370a3cd273c2abb6a237a
GonzaloUlla <gonzaulla@gmail.com>
2018-08-23 01:59:06 -0300

    Adding built-in Djang API docs

--- helpo-api/helpo/urls.py
+++ helpo-api/helpo/urls.py
@@ -5,10 +5,12 @@ from django.views.generic import TemplateView
 from rest_framework import routers
 from actividades import urls as actividades_urls
 from users import urls as users_urls
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [ 
     url(r'^admin/', admin.site.urls),
-    url(r'auth/', include('knox.urls'))
+    url(r'auth/', include('knox.urls')),
+    url(r'^docs/', include_docs_urls(title='Helpo API'))
 ]
 
 urlpatterns += actividades_urls.urlpatterns

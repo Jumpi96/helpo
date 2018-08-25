aff348f6fb535c952972c475f0e7fa214c26ad90
Luciano Gil <luchogil96@gmail.com>
2018-08-25 18:58:11 -0300

    Organizado en 3 componentes para hacerlo mas modular.

--- helpo-web/src/views/Organizaciones/OrganizacionesPage.js
+++ helpo-web/src/views/Organizaciones/OrganizacionesPage.js
@@ -1,30 +1,60 @@
 import React from 'react';
-import { Card, CardHeader, CardBody } from 'reactstrap';
-import api from '../../api';
-import './Organizaciones.css';
+import { Card, CardHeader, CardBody, Table } from 'reactstrap';
+import moment from 'moment';
+import api from '../../../api';
+import './organizaciones.css';
+import ong from '../../../assets/img/ong.png';
+import { getImagen } from '../../../utils/Imagen';
 import OrganizacionesList from './OrganizacionesList'
 
 class OrganizacionesPage extends React.Component {  
 
   constructor(props) {
     super(props);
+    const urlParams = new URLSearchParams(this.props.location.search)
+    const id = urlParams.get('id');
     this.state = {
-      organizaciones:[]
+      organizaciones: []
     }
-    this.componentDidMount = this.componentDidMount.bind(this);
+    this.toggleColaborar = this.toggleColaborar.bind(this);
+    this.loadorganizacion = this.loadorganizacion.bind(this);
   }
 
-  componentDidMount() {  // Suponiendo  que res son todas las organizaciones
-    api.get(`/perfiles/perfil_organizacion/`)
+  loadOrganizacion() { // Puede usarse 
+    api.get('/actividades/consulta_organizaciones/' + this.state.organizacion.id + '/')
+      .then(res => {
+        this.setState({ organizacion: res.data });
+      })
+      .catch((error) => {
+        if (error.response){ console.log(error.response.status) }
+        else { console.log('Error: ', error.message)}
+      })
+  }
+
+  componentDidMount() { // Ver como solucionar esto, falta la vista de la API
+    api.get(`/perfiles/perfil_organizacion/*`)
     .then( (res) => {
+      let rubro = res.rubro
+      let ubicacion = res.ubicacion
+      if ( rubro == null ) {
+        rubro = { id: 0, nombre: 'none'}
+      }
+      if ( ubicacion == null ) {
+        ubicacion = { latitud: 0, longitud: 0, notas:'#!None#!'}
+      }
       this.setState({
-        organizaciones: res.data,        
+        cuit: res.cuit,
+        telefono: res.telefono,
+        descripcion: res.descripcion,
+        rubro_id: rubro.id,
+        rubro_nombre: rubro.nombre,
+        avatar_url: res.avatar.url,        
       })
     })
   }  
 
   renderListadoOrganizaciones(){
-    const organizaciones = this.state.organizaciones;
+    const organizaciones = this.props.organizaciones;
     if(organizaciones.length === 0){
       return(
         <div className="row">
@@ -44,7 +74,9 @@ class OrganizacionesPage extends React.Component {
   }
 
 
+
   render() {
+
     return (
 
       <div className="animated fadeIn">

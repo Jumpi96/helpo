81bce23d3f3cd386234b5613bd940a53cb19e868
Luciano Gil <luchogil96@gmail.com>
2018-08-25 18:58:11 -0300

    Organizado en 3 componentes para hacerlo mas modular.

--- helpo-web/src/views/Organizaciones/OrganizacionCard.js
+++ helpo-web/src/views/Organizaciones/OrganizacionCard.js
@@ -1,33 +1,119 @@
 import React, { Component } from 'react';
-import { Card, CardBody } from 'reactstrap';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+import { Card, CardBody, Progress } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { getImagen } from '../../utils/Imagen';
+import classNames from 'classnames';
+import { mapToCssModules } from 'reactstrap/lib/utils';
+import { getImagen } from '../../../../utils/Imagen';
 
-class OrganizacionCard extends Component {
+const propTypes = {
+  header: PropTypes.string,
+  mainText: PropTypes.string,
+  smallText: PropTypes.string,
+  color: PropTypes.string,
+  value: PropTypes.string,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  cssModule: PropTypes.object,
+  variant: PropTypes.string,
+  link: PropTypes.string,
+};
+
+class organizacionCard extends Component {
+
+
+  getVoluntarios() {
+    const arrayVoluntarios = this.props.organizacion.voluntarios ? this.props.organizacion.voluntarios : [];
+    const necesidades = arrayVoluntarios.slice(0, 2).map((voluntario) => {
+      return (
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          {voluntario.funcion.nombre}
+          <span class="badge badge-primary badge-pill">{voluntario.cantidad}</span>
+        </li>
+      )
+    });
+    return necesidades;
+  }
+
+  getorganizacionPorcentaje() {
+    let contadorNecesidades = 0;
+    let contadorCubiertas = 0;
+    let contadorTemp = 0;
+    const voluntarios = this.props.organizacion.voluntarios;
+    const necesidades = this.props.organizacion.necesidades;
+    if (voluntarios && necesidades) {
+      for (let i = 0; i < voluntarios.length; i++) {
+        contadorNecesidades += voluntarios[i].cantidad;
+        contadorCubiertas += voluntarios[i].participaciones.length;
+      }
+      for (let i = 0; i < necesidades.length; i++) {
+        contadorTemp = 0;
+        contadorNecesidades += necesidades[i].cantidad;
+        for (let j = 0; j < necesidades[i].colaboraciones; i++) {
+          contadorTemp += necesidades[i].colaboraciones[j].cantidad;
+        }
+        contadorCubiertas += contadorTemp;
+      }
+    }
+    return contadorNecesidades > 0 ? (contadorCubiertas/contadorNecesidades) * 100 : 0;
+  }
+
   render() {
-    const organizacion = this.props.organizacion;
+    const { 
+      organizacion,
+      className, 
+      cssModule, 
+      children, 
+      variant, 
+      link,
+      color,
+      ...attributes } 
+      = this.props;
+
+    // demo purposes only
+    const progress = { style: '', color: color, value: this.getorganizacionPorcentaje()};
+    const card = { style: '', bgColor: '' };
+
+    if (variant === 'inverse') {
+      progress.style = 'progress-white';
+      progress.color = '';
+      card.style = 'text-white';
+      card.bgColor = 'bg-' + color;
+    }
+
+    const classes = mapToCssModules(classNames(className, card.style, card.bgColor), cssModule);
+    progress.style = classNames('progress-xs my-3', progress.style);
+
     return (
-      <Card>
+      <Card className={classes} {...attributes}>
         <CardBody>
         <div>
+
           <img
-            className="col-md-3"
-            src={getImagen(organizacion ? organizacion.usuario.avatar : undefined )}
+            src={getImagen(organizacion.organizacion ? organizacion.organizacion.avatar : undefined )}
             alt="ONG"
             style={{width:'75px', height:'75px'}} 
           />
-
-          <div>{organizacion.usuario.nombre}</div>
           
-          <div>{organizacion.descripcion}</div>
+          <div>
+            {organizacion.organizacion ?
+              organizacion.nombre + ' - ' + organizacion.organizacion.nombre : undefined
+            }
+          </div>
 
-        <div className="col-6">
-          <Link to={this.props.link}> 
-            <button className="btn btn-primary pull-right" > 
+          <div className="col-md-6">
+            <ul className="list-group">
+              {this.getNecesidades()}
+              {this.getVoluntarios()}
+            </ul>
+          </div>
+          
+          <Link to={link}> 
+            <button className="btn btn-primary pull-right" hidden={!this.props.auth}>
               + Ver perfil
             </button>
           </Link>
-        </div>
 
         </div>
         </CardBody>
@@ -36,4 +122,6 @@ class OrganizacionCard extends Component {
   }
 }
 
-export default OrganizacionCard;
+organizacionCard.propTypes = propTypes;
+
+export default organizacionCard;

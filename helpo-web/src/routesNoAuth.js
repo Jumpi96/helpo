075a150a632dfce4d6fa8bd4f87831d0b3111bb8
Luciano Gil <luchogil96@gmail.com>
2018-08-26 20:22:00 -0300

    Se muestra en el noAuth pero todavia no muestra ONGs

--- helpo-web/src/routesNoAuth.js
+++ helpo-web/src/routesNoAuth.js
@@ -17,6 +17,11 @@ const ConsultarEventosView = Loadable({
   loading: Loading,
 }) 
 
+const OrganizacionesPage = Loadable({
+  loader: () => import('./views/Organizaciones/OrganizacionesPage.js'),
+  loading: Loading,
+})
+
 const Dashboard = Loadable({
   loader: () => import('./views/Home/HomeNoAuth'),
   loading: Loading,
@@ -29,8 +34,7 @@ const routes = [
   { path: '/noAuth/dashboard', name: 'helpo', component: Dashboard },
   { path: '/noAuth/actividades/consultar-eventos', name: 'Consultar eventos', component: ConsultarEventosPage },
   { path: '/noAuth/actividades/consultar-evento/', name: 'Consultar evento', component: ConsultarEventosView },
-
-
+  { path: '/noAuth/organizaciones', name: 'Organizaciones', component: OrganizacionesPage },
 ];
 
 export default routes;

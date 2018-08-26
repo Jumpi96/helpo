e11a7c7392f9b43cc55018b38d26abdc770fb486
julieta <julirios299@gmail.com>
2018-08-27 01:15:42 +0200

    Borrado de pavadas

--- helpo-web/src/containers/DefaultLayout/DefaultHeader.js
+++ helpo-web/src/containers/DefaultLayout/DefaultHeader.js
@@ -12,7 +12,6 @@ import { getImagen } from '../../utils/Imagen';
 
 const propTypes = {
   children: PropTypes.node,
-  nombre: PropTypes.string.nombre,
 };
 
 const defaultProps = {};
@@ -44,7 +43,7 @@ class DefaultHeader extends Component {
             <NavLink href="#"><i className="icon-bell"></i><Badge pill color="danger" hidden>0</Badge></NavLink>
           </NavItem>
           <AppHeaderDropdown direction="down">
-            <DropdownToggle nav title="Julieta">
+            <DropdownToggle nav>
             {this.props.auth.user.nombre} <img src={getImagen(this.props.auth.user.avatar)} className="img-avatar" alt="admin@bootstrapmaster.com" /> {/*ACA DEBERIA IR LA FOTO DE PERFIL DEL USUARIO*/}
             </DropdownToggle>
             <DropdownMenu right style={{ right: 'auto' }}>

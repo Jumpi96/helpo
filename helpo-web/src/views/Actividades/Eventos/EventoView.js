8d88482c79fa0e7a7d0f1dbdd3589a3c3f2433e5
GonzaloUlla <gonzaulla@gmail.com>
2018-08-27 01:42:10 -0300

    Cambiando la vista de un Evento para que use el componente creado: ButtonsCompartirEvento

--- helpo-web/src/views/Actividades/Eventos/EventoView.js
+++ helpo-web/src/views/Actividades/Eventos/EventoView.js
@@ -7,7 +7,7 @@ import ModalEliminarItem from '../../common/ModalEliminarItem/ModalEliminarItem'
 import * as eventoActions from '../../../actions/eventoActions';
 import EventoForm from './EventoForm';
 import './Eventos.css';
-import MetaTags from 'react-meta-tags';
+import ButtonsCompartirEvento from '../../common/ButtonsCompartir/ButtonsCompartirEvento';
 
 class EventoView extends React.Component {
   constructor(props) {
@@ -93,17 +93,6 @@ class EventoView extends React.Component {
       }
       return (
         <div className="col-md-8 col-md-offset-2">
-          <MetaTags>
-            {
-              // Cambiar
-              // La misma URL que se comparta debe tener los siguientes meta tags
-            }
-            <meta property="og:url" content={"https://www.helpo.com.ar/#/actividades/evento/" + evento.id} />
-            <meta property="og:type" content="article" />
-            <meta property="og:title" content={"Helpo: " + evento.nombre} />
-            <meta property="og:description" content={evento.descripcion} />
-            <meta property="og:image" content="https://i.imgur.com/GyVKBfQ.jpg" />
-          </MetaTags>
           <h1>{evento.nombre}</h1>
           <div className="row">
             <div className="form-group col-md-6">
@@ -175,43 +164,8 @@ class EventoView extends React.Component {
           </div>
           <ModalEliminarItem open={this.state.showModalEliminar} nombre={this.state.evento.nombre}
             closeModal={this.confirmDeleteNecesidad} />
-          <div className="btn-group form-group" role="group">
-            {
-              // Facebook - Cambiar
-            }
-            <a target="_blank" rel="noopener noreferrer"
-              href={"https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.helpo.com.ar%2F%23%2F" +
-                "actividades%2Fevento%2F" + evento.id}>
-              <img src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png"
-                alt="Compartir en Facebook" width="40" height="40" facebook-share-dialog="true" />
-            </a>
-            {
-              // Twitter - Cambiar
-            }
-            <a target="_blank" rel="noopener noreferrer"
-              href={"http://twitter.com/share?text=Sumate%20a%20mi%20evento%20en%20Helpo%3A%20" + evento.nombre +
-                "&url=https%3A%2F%2Fwww.helpo.com.ar%2F%23%2Factividades%2Fevento%2F" + evento.id + "&hashtags=Helpo"}>
-              <img src="https://seeklogo.com/images/T/twitter-2012-negative-logo-5C6C1F1521-seeklogo.com.png"
-                alt="Compartir en Twitter" width="40" height="40" />
-            </a>
-            {
-              // Google+ - Cambiar
-            }
-            <a target="_blank" rel="noopener noreferrer"
-              href={"https://plus.google.com/share?url=https%3A%2F%2Fwww.helpo.com.ar%2F%23%2Factividades%2Fevento%2F" + evento.id}>
-              <img src="https://i.ebayimg.com/images/g/jqIAAOSwnHZYX128/s-l1600.jpg"
-                alt="Compartir en Google+" width="40" height="40" />
-            </a>
-            {
-              // LinkedIn - Cambiar
-            }
-            <a target="_blank" rel="noopener noreferrer"
-              href={"https://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fwww.helpo.com.ar%2F%23%2Factividades%2Fevento%2F" +
-                evento.id + "&summary=Sumate%20a%20mi%20evento%20en%20Helpo%3A%20" + evento.nombre + "&source=Helpo"}>
-              <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-512.png"
-                alt="Compartir en LinkedIn" width="40" height="40" />
-            </a>
-          </div>
+
+          <ButtonsCompartirEvento evento={this.state.evento} />
         </div>
       );
     } else {

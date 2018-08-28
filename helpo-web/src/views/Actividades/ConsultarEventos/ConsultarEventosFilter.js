a173f25dd06515fe1f602b5cc83695de485e0337
Jumpi96 <juampilorenzo@gmail.com>
2018-08-28 09:29:39 -0300

    Cambio en web

--- helpo-web/src/views/Actividades/ConsultarEventos/ConsultarEventosFilter.js
+++ helpo-web/src/views/Actividades/ConsultarEventos/ConsultarEventosFilter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col, Row } from 'reactstrap';
 import Select from 'react-select';
+import moment from 'moment';
 import api from '../../../api.js'
 
 class ConsultarEventosFilter extends React.Component {
@@ -11,13 +12,16 @@ class ConsultarEventosFilter extends React.Component {
       selectedFunciones: [],
       selectedMateriales: [],
       selectedRubros: [],
+      selectedFechas: [],
       optionsFunciones: [],
       optionsMateriales: [],
       optionsRubros: [],
+      optionsFechas: this.loadOptionsFecha(),
     };
     this.handleChangeFunciones = this.handleChangeFunciones.bind(this);
     this.handleChangeMateriales = this.handleChangeMateriales.bind(this);
     this.handleChangeRubros = this.handleChangeRubros.bind(this);
+    this.handleChangeFechas = this.handleChangeFechas.bind(this);
   }
 
   componentDidMount() {
@@ -48,28 +52,55 @@ class ConsultarEventosFilter extends React.Component {
     return options;
   }
 
+  loadOptionsFecha() {
+    return [
+      { value: 0, label: 'Esta semana' },
+      { value: 1, label: 'Próxima semana' },
+      { value: 2, label: 'Próximo mes' },
+    ];
+  }
+
   handleChangeFunciones(selectedFunciones) {
     this.setState({ selectedFunciones });
-    const { selectedMateriales } = this.state;
-    const { selectedRubros } = this.state;
-    this.updatePath(selectedMateriales, selectedFunciones, selectedRubros);
+    const { selectedRubros, selectedMateriales, selectedFechas } = this.state;
+    this.updatePath(selectedMateriales, selectedFunciones, selectedRubros, selectedFechas);
   }
 
   handleChangeMateriales(selectedMateriales) {
     this.setState({ selectedMateriales });
-    const { selectedFunciones } = this.state;
-    const { selectedRubros } = this.state;
-    this.updatePath(selectedMateriales, selectedFunciones, selectedRubros);
+    const { selectedFunciones, selectedRubros, selectedFechas } = this.state;
+    this.updatePath(selectedMateriales, selectedFunciones, selectedRubros, selectedFechas);
   }
 
   handleChangeRubros(selectedRubros) {
     this.setState({ selectedRubros });
-    const { selectedFunciones } = this.state;
-    const { selectedMateriales } = this.state;
-    this.updatePath(selectedMateriales, selectedFunciones, selectedRubros);
+    const { selectedFunciones, selectedMateriales, selectedFechas } = this.state;
+    this.updatePath(selectedMateriales, selectedFunciones, selectedRubros, selectedFechas);
   }
 
-  updatePath(materiales, funciones, rubros) {
+  handleChangeFechas(selectedFechas) {
+    this.setState({ selectedFechas });
+    const { selectedFunciones, selectedMateriales, selectedRubros } = this.state;
+    this.updatePath(selectedMateriales, selectedFunciones, selectedRubros, selectedFechas);
+  }
+
+  getValorFecha(fechas) {
+    const value = fechas[0].value;
+    let desde, hasta;
+    if (value === 0) {
+      desde = moment();
+      hasta = moment().add(7, 'days');
+    } else if (value === 1) {
+      desde = moment().add(7, 'days');
+      hasta = moment().add(14, 'days');
+    } else {
+      desde = moment();
+      hasta = moment().add(1, 'months');
+    }
+    return 'fecha_desde=' + desde.toISOString() + '&fecha_hasta=' + hasta.toISOString();
+  }
+
+  updatePath(materiales, funciones, rubros, fechas) {
     let ruta = '?';
     if (this.props.organizacion) {
       ruta += 'organizacion=' + this.props.organizacion + '&';
@@ -96,6 +127,9 @@ class ConsultarEventosFilter extends React.Component {
       });
       ruta = ruta.substring(0, ruta.length-1) + '&';
     }
+    if (fechas.length > 0) {
+      ruta += this.getValorFecha(fechas);
+    }
     ruta = ruta[ruta.length-1] === '&' ? ruta.substring(0, ruta.length-1) : ruta;
     this.props.updatePath(ruta);
   }
@@ -104,7 +138,7 @@ class ConsultarEventosFilter extends React.Component {
     return (
       <div>
         <Row>
-            <Col md="3">
+            <Col md="2">
                 <label for="materiales">Materiales</label>
                 <Select
                   name="materiales"
@@ -114,17 +148,17 @@ class ConsultarEventosFilter extends React.Component {
                   value={this.state.selectedMateriales}
                 />
             </Col>
-            <Col md="3">
+            <Col md="2">
                 <label for="funciones">Funciones</label>
                 <Select
                   name="funciones"
                   placeholder="Seleccione..."
                   options={this.state.optionsFunciones}
-                  isMulti onChange={this.handleChangeFunciones}
                   value={this.state.selectedFunciones}
+                  isMulti onChange={this.handleChangeFunciones}
                 />
             </Col>
-            <Col md="3">
+            <Col md="2">
               <label for="rubros">Rubros</label>
               <Select
                 name="rubros"
@@ -132,6 +166,16 @@ class ConsultarEventosFilter extends React.Component {
                 options={this.state.optionsRubros}
                 isMulti onChange={this.handleChangeRubros}
                 value={this.state.selectedRubros}
+              />
+            </Col>
+            <Col md="2">
+              <label for="fechas">Fecha</label>
+              <Select
+                name="fechas"
+                placeholder="Seleccione..."
+                options={this.state.optionsFechas}
+                onChange={this.handleChangeFechas}
+                value={this.state.selectedFechas}
               />
             </Col>
         </Row>

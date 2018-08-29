b118bfd9da489e30128221886b87a9e136f03288
Agustin Borello <agustinborello21@gmail.com>
2018-08-29 18:04:27 -0300

    Correcciones a imagenes

--- helpo-web/src/views/Perfiles/PerfilEmpresa/ModificarPerfilEmpresa.js
+++ helpo-web/src/views/Perfiles/PerfilEmpresa/ModificarPerfilEmpresa.js
@@ -244,9 +244,8 @@ class ModificarPerfilEmpresa extends Component {
     
     let avatar_url = this.props.data.avatar.url
     if ( this.state.avatar_changed ) {
-      console.log("MIAAAAAAMEEEEEEE")
-      const rx = /\/9j\/.*/gm
-      const encondedAvatar = rx.exec(this.state.avatar_url)[0]
+      const rx = /data.*base64,(.*)/gm
+      const encondedAvatar = rx.exec(this.state.avatar_url)[1]
       avatar_url = await uploadImage(encondedAvatar)
       if (avatar_url === 'recall') {
         avatar_url = await uploadImage(encondedAvatar)

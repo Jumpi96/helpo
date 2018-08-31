45ebd9fac6eae7b17b264eadf45fcec9916b0c05
GonzaloUlla <gonzaulla@gmail.com>
2018-08-30 21:20:53 -0300

    Agregado compartir eventos ONG desde Mis Eventos

--- helpo-web/src/views/common/ButtonsCompartir/ButtonsCompartirONG.js
+++ helpo-web/src/views/common/ButtonsCompartir/ButtonsCompartirONG.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import MetaTags from 'react-meta-tags';
 
-class ButtonsCompartirEvento extends Component {
+class ButtonsCompartirONG extends Component {
 
     render() {
-        const evento = this.props.evento;
+        const ong = this.props.ong;
         return (
             <div className="wrapper">
                 <MetaTags>
                     {
                         // La misma URL que se comparta debe tener los siguientes meta tags
                     }
-                    <meta property="og:url" content={"https://www.helpo.com.ar/#/redirect/evento?id=" + evento.id} />
+                    <meta property="og:url" content={"https://www.helpo.com.ar/#/redirect/ong?id=" + ong.id} />
                     <meta property="og:type" content="article" />
-                    <meta property="og:title" content={"Helpo: " + evento.nombre} />
-                    <meta property="og:description" content={evento.descripcion} />
+                    <meta property="og:title" content={ong.nombre + "en Helpo"} />
+                    <meta property="og:description" content={"Eventos de " + ong.nombre + " en Helpo"} />
                     <meta property="og:image" content="https://i.imgur.com/GyVKBfQ.jpg" />
                 </MetaTags>
                 <div className="btn-group form-group" role="group">
@@ -23,7 +23,7 @@ class ButtonsCompartirEvento extends Component {
                     }
                     <a target="_blank" rel="noopener noreferrer"
                         href={"https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.helpo.com.ar%2F%23%2F" +
-                            "redirect%2Fevento%3Fid%3D" + evento.id}>
+                            "redirect%2Fong%3Fid%3D" + ong.id}>
                         <img src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png"
                             alt="Compartir en Facebook" width="40" height="40" facebook-share-dialog="true" />
                     </a>
@@ -31,8 +31,8 @@ class ButtonsCompartirEvento extends Component {
                         // Twitter
                     }
                     <a target="_blank" rel="noopener noreferrer"
-                        href={"http://twitter.com/share?text=Sumate%20a%20este%20evento%20en%20Helpo%3A%20" + evento.nombre +
-                            "&url=https%3A%2F%2Fwww.helpo.com.ar%2F%23%2Fredirect%2Fevento%3Fid%3D" + evento.id + "&hashtags=Helpo"}>
+                        href={"http://twitter.com/share?text=Sumate%20a%20los%20eventos%20de%20" + ong.nombre + "%20en%20Helpo" +
+                            "&url=https%3A%2F%2Fwww.helpo.com.ar%2F%23%2Fredirect%2Fong%3Fid%3D" + ong.id + "&hashtags=Helpo"}>
                         <img src="https://seeklogo.com/images/T/twitter-2012-negative-logo-5C6C1F1521-seeklogo.com.png"
                             alt="Compartir en Twitter" width="40" height="40" />
                     </a>
@@ -40,7 +40,7 @@ class ButtonsCompartirEvento extends Component {
                         // Google+
                     }
                     <a target="_blank" rel="noopener noreferrer"
-                        href={"https://plus.google.com/share?url=https%3A%2F%2Fwww.helpo.com.ar%2F%23%2Fredirect%2Fevento%3Fid%3D" + evento.id}>
+                        href={"https://plus.google.com/share?url=https%3A%2F%2Fwww.helpo.com.ar%2F%23%2Fredirect%2Fong%3Fid%3D" + ong.id}>
                         <img src="https://i.ebayimg.com/images/g/jqIAAOSwnHZYX128/s-l1600.jpg"
                             alt="Compartir en Google+" width="40" height="40" />
                     </a>
@@ -48,8 +48,8 @@ class ButtonsCompartirEvento extends Component {
                         // LinkedIn
                     }
                     <a target="_blank" rel="noopener noreferrer"
-                        href={"https://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fwww.helpo.com.ar%2F%23%2Fredirect%2Fevento%3Fid%3D" +
-                            evento.id + "&summary=Sumate%20a%20este%20evento%20en%20Helpo%3A%20" + evento.nombre + "&source=Helpo"}>
+                        href={"https://www.linkedin.com/shareArticle?mini=true&url=https%3A%2F%2Fwww.helpo.com.ar%2F%23%2Fredirect%2Fong%3Fid%3D" +
+                            ong.id + "&summary=Sumate%20a%20los%20eventos%20de%20" + ong.nombre + "%20en%20Helpo&source=Helpo"}>
                         <img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-512.png"
                             alt="Compartir en LinkedIn" width="40" height="40" />
                     </a>
@@ -58,4 +58,4 @@ class ButtonsCompartirEvento extends Component {
         );
     }
 }
-export default ButtonsCompartirEvento;
+export default ButtonsCompartirONG;

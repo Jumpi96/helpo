be94d16d0da46a9ada8393af11b642bf7b1d0de5
Agustin Borello <agustinborello21@gmail.com>
2018-09-01 12:12:08 -0300

    Pruebita del admin site de django

--- helpo-api/actividades/admin.py
+++ helpo-api/actividades/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Evento
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'organizacion', 'fecha_hora_inicio', 'fecha_hora_fin', 'rubro']
+
+admin.site.register(Evento, EventoAdmin)

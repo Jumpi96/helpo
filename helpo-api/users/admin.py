4e2918ba25ce5f74fae2299fa08e3ce77c9f9d47
Agustin Borello <agustinborello21@gmail.com>
2018-09-02 20:41:42 -0300

    Agregue DjangoAdmin para appvalues

--- helpo-api/users/admin.py
+++ helpo-api/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.utils.translation import ugettext_lazy as _
 
-from .models import User
+from .models import User, AppValues
 
 
 class CustomUserAdmin(UserAdmin):
@@ -24,4 +24,9 @@ class CustomUserAdmin(UserAdmin):
     )
 
 
+class AppValuesAdmin(admin.ModelAdmin):
+    list_display = ('key', 'value')
+
+
 admin.site.register(User, CustomUserAdmin)
+admin.site.register(AppValues, AppValuesAdmin)

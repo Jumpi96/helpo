607fd82bc287073021137272c417e17b200fc0bb
Luciano Gil <luchogil96@gmail.com>
2018-09-16 00:27:20 -0300

    Pods (?

---
+++ helpo-mobile/ios/Pods/Target Support Files/Pods-helpoMobile-tvOSTests/Pods-helpoMobile-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_helpoMobile_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_helpoMobile_tvOSTests
+@end
---
+++ helpo-mobile/ios/Pods/Target Support Files/Pods-helpoMobileTests/Pods-helpoMobileTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_helpoMobileTests : NSObject
+@end
+@implementation PodsDummy_Pods_helpoMobileTests
+@end

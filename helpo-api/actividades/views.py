6e8b9f68a2c8dbcea6ff8fac3d9471f497f025be
Jumpi96 <juampilorenzo@gmail.com>
2018-09-16 12:07:21 -0300

    Funcionando retroalimentacion_ong

--- helpo-api/actividades/views.py
+++ helpo-api/actividades/views.py
@@ -416,11 +416,11 @@ class ConsultaNecesidadesReadUpdateDeleteView(RetrieveUpdateDestroyAPIView):
 def RetroalimentacionVoluntarioEvento(request):
     try:
         user = get_token_user(request)
-        colaboraciones = Colaboracion.objects.filter(voluntario_id=user).filter(necesidad_material__evento_id=request.data['evento'])
+        colaboraciones = Colaboracion.objects.filter(colaborador_id=user).filter(necesidad_material__evento_id=request.data['evento'])
         for c in colaboraciones:
             c.retroalimentacion_voluntario = True
             c.save()
-        participaciones = Participacion.objects.filter(voluntario_id=user).filter(necesidad_voluntario__evento_id=request.data['evento'])
+        participaciones = Participacion.objects.filter(colaborador_id=user).filter(necesidad_voluntario__evento_id=request.data['evento'])
         for p in participaciones:
             p.retroalimentacion_voluntario = True
             p.save()
@@ -432,14 +432,16 @@ def RetroalimentacionVoluntarioEvento(request):
 def RetroalimentacionONGEvento(request):
     try:
         voluntario = request.data['voluntario']
-        colaboraciones = Colaboracion.objects.filter(voluntario_id=voluntario).filter(necesidad_material__evento_id=request.data['evento'])
-        for c in colaboraciones:
-            c.retroalimentacion_ong = True
-            c.save()
-        participaciones = Participacion.objects.filter(voluntario_id=voluntario).filter(necesidad_voluntario__evento_id=request.data['evento'])
-        for p in participaciones:
-            p.retroalimentacion_ong = True
-            p.save()
+        if request.data['es_colaboracion']:
+            colaboraciones = Colaboracion.objects.filter(colaborador_id=voluntario).filter(necesidad_material__evento_id=request.data['evento'])
+            for c in colaboraciones:
+                c.retroalimentacion_ong = True
+                c.save()
+        else:
+            participaciones = Participacion.objects.filter(colaborador_id=voluntario).filter(necesidad_voluntario__evento_id=request.data['evento'])
+            for p in participaciones:
+                p.retroalimentacion_ong = True
+                p.save()
         return Response(request.data, status=status.HTTP_201_CREATED)
     except:
        return Response(status=status.HTTP_400_BAD_REQUEST)

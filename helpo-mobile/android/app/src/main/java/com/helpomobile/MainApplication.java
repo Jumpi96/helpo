48920ceed4387898edcf68de1a48a97c82d6b228
Jumpi96 <juampilorenzo@gmail.com>
2018-09-18 00:52:08 -0300

    Release 1.2  handle with care

--- helpo-mobile/android/app/src/main/java/com/helpomobile/MainApplication.java
+++ helpo-mobile/android/app/src/main/java/com/helpomobile/MainApplication.java
@@ -3,7 +3,6 @@ package com.helpomobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactlibrary.RNGooglePlacePickerPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -29,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGooglePlacePickerPackage(),
             new ReactNativeConfigPackage(),
             new ReactNativeOneSignalPackage(),
             new VectorIconsPackage(),

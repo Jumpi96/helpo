499844adea93788c00f0fb7372bde87afe399ce7
Gonzalo Ulla <gonzaulla@gmail.com>
2018-09-18 01:58:38 -0300

    Adding DB auto-population with initial data

--- helpo-api/entrypoint.sh
+++ helpo-api/entrypoint.sh
@@ -2,4 +2,6 @@
 
 service cron start
 ./wait-for-it.sh db:5432 -- bash -c
-python manage.py migrate && python manage.py runserver 0.0.0.0:8000
+python manage.py migrate
+python manage.py populate_db
+python manage.py runserver 0.0.0.0:8000

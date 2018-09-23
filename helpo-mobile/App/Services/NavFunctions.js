3ea2a4b58cf9d804ca44731267b0f975d705e1cf
Agustin Borello <agustinborello21@gmail.com>
2018-09-22 22:48:13 -0300

    Saque los los console.log

--- helpo-mobile/App/Services/NavFunctions.js
+++ helpo-mobile/App/Services/NavFunctions.js
@@ -13,11 +13,9 @@ export async function goVerEvento(evento, navigation) {
     const evento_response = await api.get(`actividades/eventos/${evento}/`)
     const rubros = rubros_response.data
     const eventoData = evento_response.data
-    console.tron.log("eventoData")
-    console.tron.log(eventoData)
     navigation.navigate('VerEvento', {evento: eventoData, rubros: rubros})
   }
   catch(error) {
-    console.tron.log(error)
+    console.log(error)
   }
 }

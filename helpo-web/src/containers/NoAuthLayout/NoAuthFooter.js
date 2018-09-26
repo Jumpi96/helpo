03831e06141d4f20bc14ccd0a99f24dd6af2dd29
Julieta Rios <julirios299@gmail.com>
2018-09-26 17:04:08 -0300

    Estructura de Home realizada

--- helpo-web/src/containers/NoAuthLayout/NoAuthFooter.js
+++ helpo-web/src/containers/NoAuthLayout/NoAuthFooter.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { CardFooter, Container, Row, Col } from 'reactstrap';
 import PropTypes from 'prop-types';
+import sygnet from '../../assets/img/brand/sygnet.svg'
 
 const propTypes = {
   children: PropTypes.node,
@@ -15,9 +17,21 @@ class NoAuthFooter extends Component {
 
     return (
       <React.Fragment>
-      <span><a href="https://www.helpo.com.ar">helpo</a> &copy; 2018 </span>
-      </React.Fragment>
-    );
+        <Container>
+          <Row className="py-4">
+            <Col sm={{ size: 'auto', offset: 5 }}>
+              <a className="msn-ic" href="mailto:helpoconsultas@helpo.com.ar"><i style={{color:'white'}} className="fa fa-envelope-o fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>                  
+              <a className="fb-ic" href="https://fb.me/HelpoConsultas"><i style={{color:'white'}} className="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
+              <a className="ins-ic" href="https://www.instagram.com/helpoconsultas/"><i style={{color:'white'}} className="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i></a>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={{ size: 'auto', offset: 5}}>  
+              <a style={{color:'white'}} href="https://www.helpo.com.ar">helpo</a> &copy; Copyright {(new Date().getFullYear())} 
+            </Col>
+          </Row>
+        </Container>
+      </React.Fragment>    );
   }
 }
 

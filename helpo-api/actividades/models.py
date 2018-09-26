c9b024f2ee72c2fb3eace525ea9993bbdcbfd5d3
Jumpi96 <juampilorenzo@gmail.com>
2018-09-25 23:52:19 -0300

    Cambios en modelos

--- helpo-api/actividades/models.py
+++ helpo-api/actividades/models.py
@@ -34,6 +34,7 @@ class Evento(IndexedTimeStampedModel):
     ubicacion = models.ForeignKey(Ubicacion, null=True, on_delete=models.SET_NULL)
     organizacion = models.ForeignKey(User, null=False)
     estado = models.PositiveSmallIntegerField(choices=EVENTO_STATUS, default=1, null=False, blank=False)
+    campaña = models.BooleanField(null=False, blank=False, default=False)
 
 class Contacto(models.Model):
     nombre = models.CharField(max_length=100)
@@ -49,7 +50,6 @@ class CategoriaRecurso(models.Model):
         return self.nombre
 
 class Recurso(models.Model):
-
     nombre = models.CharField(max_length=50)
     categoria = models.ForeignKey(CategoriaRecurso, null=False, on_delete=models.PROTECT)
 
@@ -96,7 +96,6 @@ class Colaboracion(models.Model):
     comentario = models.CharField(max_length=140, null=True)
     necesidad_material = models.ForeignKey(Necesidad, related_name='colaboraciones', null=False, on_delete=models.CASCADE)
     colaborador = models.ForeignKey(User, null=False)
-    entregado = models.BooleanField(null=False, blank=False, default=False)
     vigente = models.NullBooleanField(null=True, default=True)
     retroalimentacion_voluntario = models.BooleanField(default=False)
     retroalimentacion_ong = models.BooleanField(default=False)
@@ -157,3 +156,7 @@ class Propuesta(IndexedTimeStampedModel):
 
     def __str__(self):
         return self.empresa.__str__() + " - " + self.evento.__str__()
+
+class Entrega(IndexedTimeStampedModel):
+    colaboracion = models.ForeignKey(Colaboracion, related_name='entregas', null=False, on_delete=models.CASCADE)
+    cantidad = models.IntegerField(null=False)

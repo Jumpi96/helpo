5e2eba6f753e4600bad876fa431b0fcf25fcaa81
Julieta Rios <julirios299@gmail.com>
2018-09-29 15:44:02 -0300

    Avances con Home

--- helpo-web/src/views/Home/HomeNoAuth.js
+++ helpo-web/src/views/Home/HomeNoAuth.js
@@ -1,33 +1,43 @@
 import React, { Component } from 'react';
-import {CarouselCaption, CarouselControl, CarouselIndicators, CarouselItem, Carousel, Card , CardGroup, CardTitle, CardText, Button, CardBody, CardImg, CardSubtitle} from 'reactstrap';
+import {Row, Col, Collapse, CarouselCaption, CarouselControl, CarouselIndicators, CarouselItem, Carousel, Card , CardGroup, CardTitle, CardText, Button, CardBody, CardImg, CardSubtitle} from 'reactstrap';
 import './HomeNoAuth.css';
 import imagen from "../../assets/img/home-noauth-background.jpg";
+import imagen1 from "../../assets/img/Empresas.png";
+import imagen2 from "../../assets/img/ONG.png";
+import imagen3 from "../../assets/img/Voluntarios.png";
 
 const items = [
   {
     src: imagen,
   },
   {
-    src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    altText: 'Slide 2',
-    caption: 'Slide 2'
+    src: imagen2,
   },
   {
-    src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    altText: 'Slide 3',
-    caption: 'Slide 3'
+    src: imagen3,
   }
+
 ];
 
 class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { activeIndex: 0 };
+  constructor(props, context) {
+    super(props, context);
+    this.state = { activeIndex: 0, 
+      open1: false,
+      open2: false,
+      open3: false};
     this.next = this.next.bind(this);
     this.previous = this.previous.bind(this);
     this.goToIndex = this.goToIndex.bind(this);
     this.onExiting = this.onExiting.bind(this);
     this.onExited = this.onExited.bind(this);
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(num) {
+    if (num === 1) {
+    this.setState({ collapse1: !this.state.collapse1 });
+    }
   }
 
   onExiting() {
@@ -73,45 +83,57 @@ class Home extends Component {
 
     return (
       <React.Fragment>
-        <Carousel
-        activeIndex={activeIndex}
-        next={this.next}
-        previous={this.previous}
-        >
-          <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
-        {slides}
-          <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
-          <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
-        </Carousel>
-        <CardGroup style={{marginTop:"20px"}}>
-          <Card>
-            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" />
-            <CardBody>
-              <CardTitle>Card title</CardTitle>
-              <CardSubtitle>Card subtitle</CardSubtitle>
-              <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-            <CardBody>
-              <CardTitle>Card title</CardTitle>
-              <CardSubtitle>Card subtitle</CardSubtitle>
-              <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-            <CardBody>
-              <CardTitle>Card title</CardTitle>
-              <CardSubtitle>Card subtitle</CardSubtitle>
-              <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-              <Button>Button</Button>
-            </CardBody>
-          </Card>
-        </CardGroup>
+          <Carousel
+          activeIndex={activeIndex}
+          next={this.next}
+          previous={this.previous}
+          >
+            <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+            {slides}
+            <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+            <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+          </Carousel> 
+          <Row style={{ marginRight:'-70px'}}>
+            <Col>
+              <Button color="primary" onClick={() => this.setState({ open1: !this.state.open1 })} style={{fontSize:'20px', marginTop: '10px', marginBottom: '1rem', height: '150px', width: '400px'}}>¿Quiénes somos?</Button>
+              <Collapse isOpen={this.state.open1}>
+                <Card style={{width: '400px'}}>
+                  <CardBody>
+                  Anim pariatur cliche reprehenderit,
+                  enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                  anim keffiyeh helvetica, craft beer labore wes anderson cred
+                  nesciunt sapiente ea proident.
+                  </CardBody>
+                </Card>
+              </Collapse>
+            </Col>
+            <Col>
+              <Button color="primary" onClick={() => this.setState({ open2: !this.state.open2 })} style={{fontSize:'20px', marginTop: '10px', marginBottom: '1rem', height: '150px', width: '400px' }}>¿Qué hacemos?</Button>
+              <Collapse isOpen={this.state.open2}>
+                <Card style={{width: '400px'}}>
+                  <CardBody>
+                  Anim pariatur cliche reprehenderit,
+                  enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                  anim keffiyeh helvetica, craft beer labore wes anderson cred
+                  nesciunt sapiente ea proident.
+                  </CardBody>
+                </Card>
+              </Collapse>
+            </Col>
+            <Col>
+              <Button color="primary" onClick={() => this.setState({ open3: !this.state.open3 })} style={{fontSize:'20px', marginTop: '10px', marginBottom: '1rem', height: '150px', width: '400px'}}>¿Cómo participar?</Button>
+              <Collapse isOpen={this.state.open3}>
+                <Card style={{width: '400px'}}>
+                  <CardBody>
+                  Anim pariatur cliche reprehenderit,
+                  enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                  anim keffiyeh helvetica, craft beer labore wes anderson cred
+                  nesciunt sapiente ea proident.
+                  </CardBody>
+                </Card>
+              </Collapse>
+            </Col>
+          </Row>
       </React.Fragment>
     );
   }

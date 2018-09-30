fad5e7a43a5a912fc260eeb0744d4391b182698e
Jumpi96 <juampilorenzo@gmail.com>
2018-09-30 12:11:58 -0300

    Anterior

--- helpo-web/src/_navEmpresa.js
+++ helpo-web/src/_navEmpresa.js
@@ -38,15 +38,6 @@ export default {
         url: '/organizaciones',
         icon: 'icon-calendar'
       },
-      {
-        title: true,
-        name: 'Perfil',
-        wrapper: {            // optional wrapper object
-          element: '',        // required valid HTML5 element tag
-          attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
-        },
-        class: ''             // optional class names space delimited list for title item ex: "text-center"
-      },
     ],
   };
   

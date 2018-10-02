5b15b845666c19fcee9cc1ccfc0a4989fadf7825
Gonzalo Ulla <gonzaulla@gmail.com>
2018-10-02 01:14:21 -0300

    Done

--- helpo-api/users/serializers.py
+++ helpo-api/users/serializers.py
@@ -221,7 +221,7 @@ class OrganizacionProfileSerializer(serializers.ModelSerializer):
         manos = 0
         for participacion in participaciones:
             evento = participacion.necesidad_voluntario.evento
-            if not evento.id in dict_conteo:        
+            if not evento.id in dict_conteo:
                 dict_conteo[evento.id] = []
             dict_conteo[evento.id].append(participacion.colaborador)
             manos = manos + 1

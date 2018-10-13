4edb0084689a88fe95af3f46ccc54dffd72443a0
Agustin Borello <agustinborello21@gmail.com>
2018-10-13 20:09:50 -0300

    Agrego PDF consultar colaboraciones

--- helpo-web/src/views/Actividades/ConsultarColaboraciones/ConsultarColaboraciones.js
+++ helpo-web/src/views/Actividades/ConsultarColaboraciones/ConsultarColaboraciones.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Card, CardHeader, CardBody, Button, Modal, ModalBody} from 'reactstrap'
 import TablaColaboraciones from './TablaColaboraciones/TablaColaboraciones'
 import TablaVoluntarios from './TablaVoluntarios/TablaVoluntarios'
+import downloadPDF from './downloadPDF'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import * as actions from '../../../actions/consultarColaboracionesActions'
@@ -32,11 +33,18 @@ class ConsultarColaboracionConnected extends React.Component {
     this.submitChanges = this.submitChanges.bind(this);
     this.renderModal = this.renderModal.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
+    this.downloadPDF = this.downloadPDF.bind(this);
+    this.dataIsEmpty = this.dataIsEmpty.bind(this);
   }
 
   componentDidMount() {
     this.props.fetchData(this.props.match.params.eventoId)
-  } 
+  }
+
+  downloadPDF() {
+    const { necesidades, ong_nombre } = this.props
+    downloadPDF(necesidades, ong_nombre)
+  }
 
   toggleModal(bool) {
     this.setState({
@@ -66,6 +74,19 @@ class ConsultarColaboracionConnected extends React.Component {
     )
   }
 
+  dataIsEmpty() {
+    let is_empty = true
+    console.log(this.props.necesidades)
+    if (typeof this.props.necesidades === 'undefined' || typeof this.props.necesidades.necesidades === 'undefined' || typeof this.props.necesidades.voluntarios === 'undefined') {
+      return is_empty
+    }
+    if (this.props.necesidades.necesidades.length > 0 && this.props.necesidades.voluntarios.length > 0) {
+      is_empty = false
+    }
+    console.log(this.props.necesidades.necesidades.length)
+    return is_empty
+  }
+
   submitChanges() {
     this.props.sentDataSuccess(false)
     this.toggleModal(true)
@@ -92,6 +113,16 @@ class ConsultarColaboracionConnected extends React.Component {
   }
 
   render() {
+    let descargar_pdf = null
+    if (!this.dataIsEmpty()) {
+      descargar_pdf = (
+        <Button style={{marginLeft: 10}} color='primary' className='col-' onClick={this.downloadPDF}>
+          Descargar como PDF
+        </Button>
+      )
+    }
+
+    console.log(this.dataIsEmpty())
     const content = () => { return (
       <div>
       <CardHeader>
@@ -105,6 +136,8 @@ class ConsultarColaboracionConnected extends React.Component {
             <Button color='primary' className='col-' onClick={this.submitChanges}>
               Guardar cambios
             </Button>
+            {/* Boton descargar PDF */}
+            {descargar_pdf}            
             <div style={{ allignItems: 'center' }}>{this.renderModal()}</div>
             </div>
           </div>
@@ -131,7 +164,8 @@ const mapStateToProps = state => {
     entregados: state.consultarColaboraciones.entregados,
     participaciones: state.consultarColaboraciones.participaciones,
     sentSuccess: state.consultarColaboraciones.sentDataSuccess,
-    sentError: state.consultarColaboraciones.sentDataHadError
+    sentError: state.consultarColaboraciones.sentDataHadError,
+    ong_nombre: state.auth.user.nombre
    }
 }
 

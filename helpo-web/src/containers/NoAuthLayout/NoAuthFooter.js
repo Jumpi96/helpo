952bc720987c225a4bf9a8c639a83ce528b661da
julirios15 <julirios299@gmail.com>
2018-11-03 02:17:12 -0300

    Prueba home despues de tanto tiempo sin tocarlo

--- helpo-web/src/containers/NoAuthLayout/NoAuthFooter.js
+++ helpo-web/src/containers/NoAuthLayout/NoAuthFooter.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import { CardFooter, Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import PropTypes from 'prop-types';
-import sygnet from '../../assets/img/brand/sygnet.svg'
 
 const propTypes = {
   children: PropTypes.node,

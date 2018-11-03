428db77ff4da3fd55ffca1f480bdfeb610858bd1
Jumpi96 <juampilorenzo@gmail.com>
2018-11-03 18:22:28 -0300

    2nd attempt

--- helpo-api/recommendations/views.py
+++ helpo-api/recommendations/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from recommendations.services import get_data_fecha_regressor, train_fecha_regressor, predict_fechas, train_evento_recommendations
+from recommendations.services import get_data_fecha_regressor, train_fecha_regressor, predict_fechas, train_evento_recommendations, handle_predict_fechas
 from common.functions import get_token_user
 
 @api_view(['POST'])
 def Train(request):
     try:
         #train_evento_recommendations()
-        train_fecha_regressor()
+        #train_fecha_regressor()
         return Response(status=status.HTTP_200_OK)
     except:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def view_predict_fechas(request):
-    try:
+    #try:
         ong = get_token_user(request)
         return Response(handle_predict_fechas(request.data, ong), status=status.HTTP_200_OK)
-    except:
+    #except:
         return Response(status=status.HTTP_400_BAD_REQUEST)

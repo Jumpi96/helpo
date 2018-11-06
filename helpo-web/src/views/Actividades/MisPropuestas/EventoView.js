7b0b99ed4a946ae48fc3da9f580a1f999553ad4a
Jumpi96 <juampilorenzo@gmail.com>
2018-11-06 16:00:38 -0300

    Fixed botones

--- helpo-web/src/views/Actividades/MisPropuestas/EventoView.js
+++ helpo-web/src/views/Actividades/MisPropuestas/EventoView.js
@@ -295,14 +295,12 @@ class EventoView extends React.Component {
             this.getComentario(propuesta) : undefined
           }
           {moment(evento.fecha_hora_inicio) < moment() && propuesta.aceptado !== -1 ?
-            <div className="btn-group form-group" role="group">
-              <button
-                onClick={this.toggleView}
-                className="btn btn-warning"
-              >
-                Comentar actividad social
+            <button
+              onClick={this.toggleView}
+              className="btn btn-warning"
+            >
+              Comentar actividad social
               </button>
-            </div>
             : undefined
           }
           <ModalConfirmarEdicion
@@ -311,7 +309,7 @@ class EventoView extends React.Component {
         </div>
       );
     } else {
-      return <div className="loader"/>
+      return <div className="loader" />
     }
   }
 };

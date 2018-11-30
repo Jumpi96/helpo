d6c4ec7a6d2795b0ccc37a38b726f6eee916fdea
Jumpi96 <juampilorenzo@gmail.com>
2018-11-30 07:57:46 -0300

    Fixing CI

--- helpo-web/src/containers/DefaultLayout/DefaultHeader.js
+++ helpo-web/src/containers/DefaultLayout/DefaultHeader.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem } from 'reactstrap';
+import { DropdownItem, DropdownMenu, DropdownToggle, Nav } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';

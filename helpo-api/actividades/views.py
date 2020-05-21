e1e667801f2f2bbc1a903615b6dc176405fa94cb
Jumpi96 <juampilorenzo@gmail.com>
2020-05-21 20:16:20 -0300

    Fix bug consulta_eventos without token

--- helpo-api/actividades/views.py
+++ helpo-api/actividades/views.py
@@ -249,7 +249,7 @@ class EventoVoluntarioCreateReadView(ListCreateAPIView):
         return eventos
 
 
-class ConsultaEventosOrganizacionCreateReadView(AuthTokenMixin, ListCreateAPIView):
+class ConsultaEventosOrganizacionCreateReadView(ListCreateAPIView):
     """
     API endpoint para ver todos los eventos próximos
     """

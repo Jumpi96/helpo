ff17b556fee50311cacf1c0f0e5467b38f6f229e
Jumpi96 <juampilorenzo@gmail.com>
2020-06-21 10:38:51 -0300

    Front-end ready

--- helpo-web/src/views/Voluntarios/VoluntariosList.js
+++ helpo-web/src/views/Voluntarios/VoluntariosList.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Col, Row } from 'reactstrap';
+import { Col, Row, CardBody } from 'reactstrap';
 import VoluntarioCard from './VoluntarioCard';
+import VoluntariosFilter from './VoluntariosFilter';
+import api from '../../api';
+
 
 class VoluntariosList extends React.Component {
   constructor(props) {
@@ -8,10 +11,16 @@ class VoluntariosList extends React.Component {
     this.state = {
       nombre: '',
       error: '',
+      voluntarios: []
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.buscarVoluntarios = this.buscarVoluntarios.bind(this);
     this.getLink = this.getLink.bind(this);
+    this.loadVoluntarios = this.loadVoluntarios.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadVoluntarios("");
   }
 
   handleInputChange(event) {
@@ -23,7 +32,8 @@ class VoluntariosList extends React.Component {
     });
   }
 
-  buscarVoluntarios(voluntarios) {
+  buscarVoluntarios() {
+    const {voluntarios} = this.state;
     if (this.state.nombre !== '') {
       const voluntariosFiltrados = voluntarios.filter(voluntario => this.contieneNombre(voluntario));
       return voluntariosFiltrados;
@@ -39,35 +49,63 @@ class VoluntariosList extends React.Component {
     return '/perfil/' + voluntario.usuario.id;
   }
 
+  loadVoluntarios(ruta) {
+    this.setState({ isLoading: true });
+    api.get('/perfiles/perfil_voluntario/' + ruta)
+      .then((res) => {
+        this.setState({ voluntarios: res.data, isLoading: false });
+      })
+      .catch((error) => {
+        if (error.response) { console.log(error.response.status) }
+        else { console.log('Error: ', error.message) }
+        this.setState({ isLoading: false });
+      })
+  }
+
   render() {
-    const voluntarios = this.buscarVoluntarios(this.props.voluntarios);
+    const voluntarios = this.buscarVoluntarios();
     return (
-      <div className="row">
-        <div className="form-group col-md-4">
-          <label for="nombre">Buscar un voluntario</label>
-          <input
-            type="text"
-            name="nombre"
-            className="form-control"
-            placeholder={"Ingrese el nombre de un voluntario"}
-            value={this.state.nombre}
-            onChange={this.handleInputChange}
-          />
-          <span style={{ color: 'red' }}>{this.state.error}</span>
+      <div>
+        <div className="row">
+          <CardBody>
+            <VoluntariosFilter
+              updatePath={this.loadVoluntarios}
+            />
+          </CardBody>
         </div>
-        <div className="col-md-8">
-          {voluntarios.map(voluntario =>
-            <Row>
-              <Col>
-                <VoluntarioCard
-                  voluntario={voluntario}
-                  key={voluntario.id} footer
-                  color="primary" auth={this.props.auth}
-                  link={this.getLink(voluntario)} // Ver que link va
-                />
-              </Col>
-            </Row>
-          )}
+        <div className="row">
+          <div className="form-group col-md-4">
+            <label for="nombre">Buscar un voluntario</label>
+            <input
+              type="text"
+              name="nombre"
+              className="form-control"
+              placeholder={"Ingrese el nombre de un voluntario"}
+              value={this.state.nombre}
+              onChange={this.handleInputChange}
+            />
+            <span style={{ color: 'red' }}>{this.state.error}</span>
+          </div>
+          <div className="col-md-8">
+            {voluntarios.length > 0 ? voluntarios.map(voluntario =>
+              <Row>
+                <Col>
+                  <VoluntarioCard
+                    voluntario={voluntario}
+                    key={voluntario.id} footer
+                    color="primary" auth={this.props.auth}
+                    link={this.getLink(voluntario)} // Ver que link va
+                  />
+                </Col>
+              </Row>
+            ) :
+              <div className="row">
+                <div className="form-group col-md-6 col-md-offset-3">
+                  <label>&emsp;Todav&iacute;a no hay voluntarios registrados.</label>
+                </div>
+              </div>
+            }
+          </div>
         </div>
       </div>
     );
--- helpo-web/src/views/Voluntarios/VoluntariosPage.js
+++ helpo-web/src/views/Voluntarios/VoluntariosPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Card, CardHeader, CardBody } from 'reactstrap';
-import api from '../../api';
 import './Voluntarios.css';
 import VoluntariosList from './VoluntariosList'
 
@@ -11,39 +10,7 @@ class VoluntariosPage extends React.Component {
     this.state = {
       voluntarios: []
     }
-    this.componentDidMount = this.componentDidMount.bind(this);
   }
-
-  componentDidMount() {
-    let url = `/perfiles/perfil_voluntario/`;
-    api.get(url)
-      .then((res) => {
-        this.setState({
-          voluntarios: res.data,
-        })
-      })
-  }
-
-  renderListadoVoluntarios() {
-    const voluntarios = this.state.voluntarios;
-    if (voluntarios.length === 0) {
-      return (
-        <div className="row">
-          <div className="form-group col-md-6 col-md-offset-3">
-            <label>&emsp;Todav&iacute;a no hay voluntarios registrados.</label>
-          </div>
-        </div>
-      )
-    }
-    else {
-      return (
-        <CardBody>
-          <VoluntariosList voluntarios={voluntarios} />
-        </CardBody>
-      )
-    }
-  }
-
 
   render() {
     return (
@@ -53,7 +20,9 @@ class VoluntariosPage extends React.Component {
           <CardHeader>
             <i className="fa fa-align-justify"></i> Voluntarios
           </CardHeader>
-          {this.renderListadoVoluntarios()}
+          <CardBody>
+            <VoluntariosList />
+          </CardBody>
         </Card>
       </div>
 
